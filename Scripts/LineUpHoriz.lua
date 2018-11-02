24cdc5621c6eb66de827d280f133bd1e908c9acb
Alex <barney@server.local>
2018-11-02 15:34:54 +0300

    first commit

---
+++ Scripts/LineUpHoriz.lua
@@ -0,0 +1,28 @@
+fl = comp.CurrentFrame.FlowView
+
+comp:StartUndo("Align Tools Horizontally")
+comp:Lock()
+selected_tools = comp:GetToolList(true)
+active_tool = comp.ActiveTool
+
+if (active_tool == nil) then
+	active_tool = selected_tools[1]
+end
+
+active_X,active_y = fl:GetPos(active_tool)
+
+for pos, tool in pairs(selected_tools) do
+	if (tool ~= active_tool) then
+		_TEMP_X, _TEMP_Y = fl:GetPos(tool)
+		-- check if nodes have the same X value
+		if _TEMP_X == active_X then
+			newpos = _TEMP_X + (pos - 1)
+		else
+			newpos = _TEMP_X
+		end
+		fl:SetPos(tool, newpos, active_y)
+	end
+end
+
+comp:Unlock()
+comp:EndUndo()
---
+++ Scripts/LineUpVert.lua
@@ -0,0 +1,25 @@
+fl = comp.CurrentFrame.FlowView
+
+comp:StartUndo("Stack Tools")
+comp:Lock()
+_SEL = comp:GetToolList(true)
+_A = comp.ActiveTool
+if (_A == nil) then
+	_A = _SEL[1]
+end
+_AX,_AY = fl:GetPos(_A)
+
+for k,v in pairs(_SEL) do
+	if (v ~= _A) then
+		_TEMP_X, _TEMP_Y = fl:GetPos(v)
+		-- check if nodes have the same X value
+		if _TEMP_Y == _AY then
+			newpos = _TEMP_Y + (k-1)
+		else
+			newpos = _TEMP_Y
+		end
+		fl:SetPos(v, _AX, newpos)
+	end
+end
+comp:Unlock()
+comp:EndUndo()
