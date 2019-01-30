60297d298b07fc62c9562e08aeed21aec9e7868c
movalex <mail@abogomolov.com>
2019-01-31 01:02:16 +0300

    process multiple savers

--- Scripts/loader_from_saver.lua
+++ Scripts/loader_from_saver.lua
@@ -1,11 +1,15 @@
-if not comp.ActiveTool then
-    print('select tool first')
+savers = comp:GetToolList(true, 'Saver')
+
+if (#savers) == 0 then
+    print('select saver tool first')
 else
-    composition:Lock()
+    comp:Lock()
     local flow = comp.CurrentFrame.FlowView
-    local x, y = flow:GetPos(comp.ActiveTool)
-    selected = comp.ActiveTool:GetAttrs()["TOOLST_Clip_Name"][1]
-    local loader = comp:Loader ({Clip = selected})
-    flow:SetPos(loader, x+1, y)
-    composition:Unlock()
+    for n, tool in ipairs(savers) do
+        x, y = flow:GetPos(tool)
+        selected_clipname = tool:GetAttrs()["TOOLST_Clip_Name"][1]
+        local loader = comp:Loader({Clip = selected_clipname})
+        flow:SetPos(loader, x+1, y)
+    end
+    comp:Unlock()
 end
