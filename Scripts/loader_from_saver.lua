d0bc593b5f9072c5c2f66916629ca18b9be6a2b6
movalex <mail@abogomolov.com>
2019-02-04 01:25:56 +0300

    handle empty padding

--- Scripts/loader_from_saver.lua
+++ Scripts/loader_from_saver.lua
@@ -8,8 +8,19 @@ else
     for n, tool in ipairs(savers) do
         x, y = flow:GetPos(tool)
         selected_clipname = tool:GetAttrs()["TOOLST_Clip_Name"][1]
-        local loader = comp:Loader({Clip = selected_clipname})
-        flow:SetPos(loader, x+1, y)
+        seq = string.match(selected_clipname, '(%d+)%..+$')
+        -- if pattern 0000.ext$ is found set selected name
+        -- otherwice replace extention with 0000-extention
+        if seq then
+            local loader = comp:Loader({Clip = selected_clipname})
+            flow:SetPos(loader, x+1, y)
+        else
+            name, ext = string.match(selected_clipname,'([^/]-([^.]+))$')
+            new_name = selected_clipname:gsub('.'..ext, '0000.'..ext)
+            local loader = comp:Loader({Clip = new_name})
+            flow:SetPos(loader, x+1, y)
+        end
     end
     comp:Unlock()
 end
+
