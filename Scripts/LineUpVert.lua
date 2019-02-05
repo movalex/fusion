51efd9cea8020bd06287f3ee25a179f2bd775d3a
Alexey Bogomolov <abogomolov@mac.com>
2019-02-05 21:56:13 +0300

    add round function for Y values

--- Scripts/LineUpVert.lua
+++ Scripts/LineUpVert.lua
@@ -1,25 +1,32 @@
 fl = comp.CurrentFrame.FlowView
 
+function _round(num, n) 
+    local mult = 10^(n or 0)
+    return math.floor(num*mult+0.5)/mult
+end
+
 comp:StartUndo("Stack Tools Vertically")
 comp:Lock()
-_SEL = comp:GetToolList(true)
-_A = comp.ActiveTool
-if (_A == nil) then
-	_A = _SEL[1]
+selected_tools = comp:GetToolList(true)
+active_tool = comp.ActiveTool
+if active_tool == nil then
+    active_tool = selected_tools[1]
+    print('active tool'.. active_tool.Name)
 end
-_AX,_AY = fl:GetPos(_A)
-
-for k,v in pairs(_SEL) do
-	if (v ~= _A) then
-		_TEMP_X, _TEMP_Y = fl:GetPos(v)
-		-- check if nodes have the same X value
-		if _TEMP_Y == _AY then
-			newpos = _TEMP_Y + (k-1)
-		else
-			newpos = _TEMP_Y
-		end
-		fl:SetPos(v, _AX, newpos)
-	end
+active_X, active_Y = fl:GetPos(active_tool)
+print(active_X..':'..active_Y)
+for k,v in pairs(selected_tools) do
+    if (v ~= active_tool) then
+        _TEMP_X, _TEMP_Y = fl:GetPos(v)
+        print(_round(_TEMP_Y,2)..' '.._round(active_Y,2))
+        -- check if nodes have the same Y value
+        if _round(_TEMP_Y, 2) == _round(active_Y,2) then 
+            newpos = _TEMP_Y + (k-1)
+        else
+            newpos = _TEMP_Y
+        end
+        fl:SetPos(v, active_X, newpos)
+    end
 end
 comp:Unlock()
 comp:EndUndo()
