c4837ab50d07e7da316ec0a4edc1b1dc7081d543
movalex <mail@abogomolov.com>
2019-03-18 23:49:25 +0300

    fix patern search

--- Scripts/Comp/ChangePath.lua
+++ Scripts/Comp/ChangePath.lua
@@ -17,30 +17,39 @@
 -- update for Fu8 michael vorberg mv@empty98.de
 -- v2.2 changes: add "process selected nodes only" for Loaders, Proxy and Savers
 --               works for multiple Savers
--- v2.3 changes: add pattern search. Add replace filepath with up to 2 matches. 
---               To search for 'v22' put 'v22' or 'v%d%d' into search field. 
---               In the latter case script will match any 'v' letter with two digits after
--- Short pattern cheatsheet:
---       .  represents any single character
---       %a represents all letters A-Z and a-z
---       %d represents all digits 0-9
---       %p represents all punctuation characters or symbols such as . , ? ! : ; @ [ ] _ { } ~
---       %s represents all white space characters such as Tab, Carr.Return, Linefeed, Space, etc
---       %w represents all alphanumeric characters A-Z and a-z and 0-9
--- https://www.fhug.org.uk/wiki/wiki/doku.php?id=plugins:understanding_lua_patterns
---              Alex Bogomolov mail@abogomolov.com
--- v 2.4 changes: Save preferences with SetData, do not lowercase path, maintain trim for Loaders 
--- (these were fixed in 2011-01-28, by Stefan Ihringer) 
+-- v3.0, 2011-01-28 by Stefan Ihringer:
+--    * option to remember search strings in globals
+--    * don't lowercase whole path name
+--    * trim in and trim are no longer reset
+-- v4.0, 2018-02-05 by Bryan Ray:
+--    * Updated for Fusion 9
+-- v4.0.1, 2018-02-07:
+--    * Updated preferences code with SetData()
+-- v4.1, 2018-11-01:
+--      use gsub for pattern search.
+--          Add replace filepath with up to 2 matches. 
+--          To search for 'v22' put 'v22' or 'v%d%d' into search field. 
+--      Short pattern cheatsheet:
+--          .  represents any single character
+--          %a represents all letters A-Z and a-z
+--          %d represents all digits 0-9
+--          %p represents all punctuation characters or symbols such as . , ? ! : ; @ [ ] _ { } ~
+--          %s represents all white space characters such as Tab, Carr.Return, Linefeed, Space, etc
+--          %w represents all alphanumeric characters A-Z and a-z and 0-9
+--      More: https://www.fhug.org.uk/wiki/wiki/doku.php?id=plugins:understanding_lua_patterns
+--
 ------------------------------------------------------
 
 function conform(filepath)
+    print(filepath)
     local matched = string.match(filepath, srchFor)
     print("found pattern: ", matched)
     if matched == nil then
         return nil
     end
     -- build the new filename using gsub
-    newclip = string.gsub(filepath, matched, srchTo)
+    print(srchFor..' '..srchTo)
+    newclip = string.gsub(filepath, srchFor, srchTo)
     print("New file path is: ", newclip)            
     return newclip
 end
@@ -54,7 +63,7 @@ if prefs then
 	doSavers = prefs.Savers or 0
 	doProxy = prefs.Proxy or 0
     doValid = prefs.Valid or 0
-    -- doProcessSelected = prefs.Valid or 0
+    doProcessSelected = prefs.ProcessSelected or 0
     doGeoLoaders = prefs.GeoLoaders or 0
 else
     lastSource = ""
@@ -63,7 +72,7 @@ else
     doSavers = 0
     doProxy = 0
     doValid = 0
-    -- doProcessSelected = prefs.Valid or 0
+    doProcessSelected = 0
     doGeoLoaders = 0
 end
 
@@ -74,9 +83,9 @@ d[2] = {"Proxy", "Checkbox", Name = "Proxy", NumAcross = 3, Default = doProxy}
 d[3] = {"Savers", "Checkbox", Name = "Savers", NumAcross = 3, Default = doSavers}
 d[4] = {"GeoLoaders", "Checkbox", Name = "GeoLoaders", NumAcross = 3, Default = doGeoLoaders}
 d[5] = {"Source", "Text", Name = "Enter pattern to search for", Default = lastSource}
-d[6] = {"Replacement", "Text", Name = "Enter the replacement path", Default = lastReplacement}
+d[6] = {"Replacement", "Text", Name = "Enter the replacement", Default = lastReplacement}
 d[7] = {"Valid", "Checkbox", Name = "Check If New Path is Valid", Default = doValid}
-d[8] = {"ProcessSelected", "Checkbox", Name = "Process only selected nodes", Default = 0}
+d[8] = {"ProcessSelected", "Checkbox", Name = "Process only selected nodes", Default = doProcessSelected}
 d[9] = {"Remember", "Checkbox", Name = "Remember options for next time", Default = 1}
 
 
@@ -86,6 +95,11 @@ x = composition:AskUser("Repath Tool", d)
 if x then
    srchFor = bmd.trim(x.Source)
    srchTo = bmd.trim(x.Replacement)
+   -- srchFor = string.gsub(x.Source, "%s+$", "")
+   -- srchFor = string.gsub(srchFor, "^%s+", "")
+   -- srchTo = string.gsub(x.Replacement, "%s+$", "")
+   -- srchTo = string.gsub(srchTo, "^%s+", "")
+
 else
    return nil
 end
@@ -103,14 +117,14 @@ end
 -- remember strings for next time
 
 if x.Remember == 1 then
-	print("Saving Preferences")
+	-- print("Saving Preferences")
 	fusion:SetData("changePath.Source", x.Source)
 	fusion:SetData("changePath.Replacement", x.Replacement)
 	fusion:SetData("changePath.Loaders", x.Loaders)
 	fusion:SetData("changePath.Savers", x.Savers)
 	fusion:SetData("changePath.Proxy", x.Proxy)
 	fusion:SetData("changePath.Valid", x.Valid)
-	-- fusion:SetData("changePath.ProcessSelected", x.ProcessSelected)
+    fusion:SetData("changePath.ProcessSelected", x.ProcessSelected)
 	fusion:SetData("changePath.GeoLoaders", x.GeoLoaders)
 end
 
@@ -145,74 +159,76 @@ toollist = composition:GetToolList(selected_checked())
 
 
 for i, tool in ipairs(toollist) do
-   tool_a = tool:GetAttrs()
+    tool_a = tool:GetAttrs()
 
-   -- process only loaders and savers
-   
-   if tool_a.TOOLS_RegID == "Loader" then
-      
-      clipTable = tool_a.TOOLST_Clip_Name
-      altclipTable = tool_a.TOOLST_AltClip_Name
-      startTime = tool_a.TOOLNT_Clip_Start
-      trimIn = tool_a.TOOLIT_Clip_TrimIn
-      trimOut = tool_a.TOOLIT_Clip_TrimOut
-      
-      -- pass a function the filename, get the newclip back, or nil
+    if tool_a.TOOLS_RegID == "Loader" then
+        
+        clipTable = tool_a.TOOLST_Clip_Name
+        altclipTable = tool_a.TOOLST_AltClip_Name
+        startTime = tool_a.TOOLNT_Clip_Start
+        trimIn = tool_a.TOOLIT_Clip_TrimIn
+        trimOut = tool_a.TOOLIT_Clip_TrimOut
+        
+        -- pass a function the filename, get the newclip back, or nil
 
-      if x.Loaders == 1 then
-         for i = 1, table.getn(clipTable) do
-            newclip = conform(clipTable[i])
-            
-            if newclip ~= nil then
-               if (fileexists(composition:MapPath(newclip)) == false) and (x.Valid == 1) then
-                  print( "FAILED : New clip does not exist; skipping sequence.\n   " .. newclip)
-               else
-                  tool.Clip[startTime[i]] = newclip
-                  tool.ClipTimeStart[startTime[i]] = trimIn[i]
-                  tool.ClipTimeEnd[startTime[i]] = trimOut[i]
-               end
+        if x.Loaders == 1 then
+            for i = 1, table.getn(clipTable) do
+                newclip = conform(clipTable[i])
+                
+                if newclip then
+                    if fileexists(composition:MapPath(newclip)) == false and x.Valid == 1 then
+                        print( "FAILED : New clip does not exist; skipping sequence.\n")
+                    else
+                        tool.Clip[startTime[i]] = newclip
+                        tool.ClipTimeStart[startTime[i]] = trimIn[i]
+                        tool.ClipTimeEnd[startTime[i]] = trimOut[i]
+                    end
+                end
             end
-         end
-      end
-      
-      -- pass a function the filename, get the newclip back, or nil
+        end
+        
+        -- pass a function the filename, get the newclip back, or nil
 
-      if x.Proxy == 1 then
-         for i = 1, table.getn( altclipTable ) do
-            if altclipTable[i] ~= "" then
-               newclip = conform( altclipTable[i] )
-      
-               if newclip ~= nil then
-                  if (fileexists(composition:MapPath(newclip)) == false) and (x.Valid == 1) then
-                     print("FAILED : New proxy clip does not exist; skipping sequence.\n " .. newclip)
-                  else
-                     tool.ProxyFilename[startTime[i]] = newclip
-                  end
-               end
+        if x.Proxy == 1 then
+            for i = 1, table.getn(altclipTable) do
+                if altclipTable[i] ~= "" then
+                newclip = conform(altclipTable[i])
+        
+                if newclip then
+                    if fileexists(composition:MapPath(newclip) == false) and x.Valid == 1 then
+                        print("FAILED : New proxy clip does not exist; skipping sequence.\n")
+                    else
+                        tool.ProxyFilename[startTime[i]] = newclip
+                    end
+                end
+                end
             end
-         end
-      end
-   end
+        end
+    end
    
-   if tool_a.TOOLS_RegID == "Saver" and x.Savers == 1 then
-      saverTable = tool_a.TOOLST_Clip_Name 
-      for i = 1, table.getn(saverTable) do
-         newsaver = conform(tool_a.TOOLST_Clip_Name[i])
-         if newsaver ~= nil then
-            tool.Clip[fu.TIME_UNDEFINED] = newsaver
-         end
-      end    
-   end
+    if tool_a.TOOLS_RegID == "Saver" and x.Savers == 1 then
+        saverTable = tool_a.TOOLST_Clip_Name 
+        for i = 1, table.getn(saverTable) do
+            newsaver = conform(tool_a.TOOLST_Clip_Name[i])
+            if newsaver ~= nil then
+                tool.Clip[fu.TIME_UNDEFINED] = newsaver
+            end
+        end    
+    end
 
-   -- process mesh loaders 
+    -- process mesh loaders 
 
-   if tool_a.TOOLS_RegID == "SurfaceFBXMesh" and x.GeoLoaders == 1 then   
-      old_name = tool.ImportFile[1]
-      newclip = conform(old_name)
-      if newclip ~= nil then
-         tool.ImportFile[fu.TIME_UNDEFINED] = newclip
-      end
-   end
+    if tool_a.TOOLS_RegID == "SurfaceFBXMesh" and x.GeoLoaders == 1 then   
+        old_name = tool.ImportFile[1]
+        newclip = conform(old_name)
+        if newclip ~= nil then
+            tool.ImportFile[fu.TIME_UNDEFINED] = newclip
+        end
+    end
+
+    if tool.ID =='MediaIn' and x.Loaders == 1 then
+        print('The tool does not work with Resolve\'s MediaIns')
+    end
 end
 
 -------------------------
