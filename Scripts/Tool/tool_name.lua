15ba98700a9a133121af716fdb6c334534623354
Alexey Bogomolov <abogomolov@mac.com>
2019-03-28 20:02:17 +0300

    update tool_name description

--- Scripts/Tool/tool_name.lua
+++ Scripts/Tool/tool_name.lua
@@ -1,2 +1,4 @@
--- tool script to get tool's ID
+-- tool script to get tool's ID. Useful if you use a ForSlash script a lot, 
+-- and don't want to keep in memory, that Resize tool is actually a 
+-- 'BetterResize', and MaskPaint is actually a 'PaintMask'.
 print(tool.ID)
