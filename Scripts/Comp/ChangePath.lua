2fa58ff34afc5eb45f09b4ce3e549631aac21fa2
movalex <mail@abogomolov.com>
2019-03-29 00:37:20 +0300

    update changepath to work in Resolve

--- Scripts/Comp/ChangePath.lua
+++ Scripts/Comp/ChangePath.lua
@@ -47,7 +47,7 @@ function conform(filepath)
         return nil
     end
     -- build the new filename using gsub
-    newclip = string.gsub(filepath, srchFor, srchTo)
+    newclip = string.gsub(filepath, matched, srchTo)
     print("New file path is: ", newclip)            
     return newclip
 end
@@ -81,7 +81,7 @@ d[2] = {"Proxy", "Checkbox", Name = "Proxy", NumAcross = 3, Default = doProxy}
 d[3] = {"Savers", "Checkbox", Name = "Savers", NumAcross = 3, Default = doSavers}
 d[4] = {"GeoLoaders", "Checkbox", Name = "GeoLoaders", NumAcross = 3, Default = doGeoLoaders}
 d[5] = {"Source", "Text", Name = "Enter pattern to search for", Default = lastSource}
-d[6] = {"Replacement", "Text", Name = "Enter the replacement", Default = lastReplacement}
+d[6] = {"Replacement", "Text", Name = "Enter the replacement path", Default = lastReplacement}
 d[7] = {"Valid", "Checkbox", Name = "Check If New Path is Valid", Default = doValid}
 d[8] = {"ProcessSelected", "Checkbox", Name = "Process only selected nodes", Default = doProcessSelected}
 d[9] = {"Remember", "Checkbox", Name = "Remember options for next time", Default = 1}
@@ -90,14 +90,19 @@ d[9] = {"Remember", "Checkbox", Name = "Remember options for next time", Default
 x = composition:AskUser("Repath Tool", d)
 
 -- did we get a response, or did they cancel
-if x then
-   srchFor = bmd.trim(x.Source)
-   srchTo = bmd.trim(x.Replacement)
-   -- srchFor = string.gsub(x.Source, "%s+$", "")
-   -- srchFor = string.gsub(srchFor, "^%s+", "")
-   -- srchTo = string.gsub(x.Replacement, "%s+$", "")
-   -- srchTo = string.gsub(srchTo, "^%s+", "")
+if bmd.trim then
+    trim = bmd.trim
+else 
+    function trim(str)
+        str = string.gsub(str, "^(%s+)", "")
+        str = string.gsub(str, "(%s+)$", "")
+        return str
+    end
+end
 
+if x then
+   srchFor = trim(x.Source)
+   srchTo = trim(x.Replacement)
 else
    return nil
 end
