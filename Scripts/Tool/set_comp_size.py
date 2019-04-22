e32af322fa2dba873a63e72d73194e71fb3c0455
Alex Bogomolov <mail@abogomolov.com>
2019-04-23 01:34:03 +0300

    add set pixel aspect ratio based on Loader

--- Scripts/Tool/set_comp_size.py
+++ Scripts/Tool/set_comp_size.py
@@ -1,7 +1,29 @@
-width = tool.GetAttrs('TOOLI_ImageWidth')
-height = tool.GetAttrs('TOOLI_ImageHeight')
+def main(tool):
+    attrs = tool.GetAttrs()
+    width = attrs['TOOLIT_Clip_Width'][1]
+    height = attrs['TOOLIT_Clip_Height'][1]
 
-comp.SetPrefs({'Comp.FrameFormat.Width': width, 'Comp.FrameFormat.Height': height})
+    comp.SetPrefs({'Comp.FrameFormat.Width': width, 'Comp.FrameFormat.Height': height})
 
-print('Comp size is adjusted to {}x{}'.format(int(width), int(height)))
+    print('Comp size is adjusted to {}x{}'.format(int(width), int(height)))
 
+    if attrs['TOOLIT_Clip_AspectMode'][1] == 2.0:
+        try:
+            aspect_x = attrs['TOOLN_ImageAspectX']
+            aspect_y = attrs['TOOLN_ImageAspectY']
+        except KeyError:
+            print('To set pixel aspect ratio, add the Loader to the viewer')
+            aspect_x = aspect_y = None
+        if aspect_x and aspect_y:
+            comp.SetPrefs({'Comp.FrameFormat.AspectX': aspect_x, 'Comp.FrameFormat.AspectY': aspect_y})
+            print('Comp aspect is changed to {}:{}'.format(aspect_x, aspect_y))
+    else:
+        print('please set Custom pixel aspect ratio for the Loader')
+
+    comp.SetPrefs({'Comp.FrameFormat.Name': 'Set by Script'})
+
+
+if tool.ID == 'Loader':
+    main(tool)
+else:
+    print('this tool script works with Loaders only')
