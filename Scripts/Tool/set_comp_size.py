c9e4bdcc8233aa2cc6545b3a885e9a0c2ff50cad
Alex Bogomolov <mail@abogomolov.com>
2019-04-23 03:10:01 +0300

    do not check custom aspect

--- Scripts/Tool/set_comp_size.py
+++ Scripts/Tool/set_comp_size.py
@@ -7,16 +7,15 @@ def main(tool):
 
     print('Comp size is adjusted to {}x{}'.format(int(width), int(height)))
 
-    if attrs['TOOLIT_Clip_AspectMode'][1] == 2.0:
-        try:
-            aspect_x = attrs['TOOLN_ImageAspectX']
-            aspect_y = attrs['TOOLN_ImageAspectY']
-        except KeyError:
-            print('To set pixel aspect ratio, add the Loader to the viewer')
-            aspect_x = aspect_y = None
-        if aspect_x and aspect_y:
-            comp.SetPrefs({'Comp.FrameFormat.AspectX': aspect_x, 'Comp.FrameFormat.AspectY': aspect_y})
-            print('Comp aspect is changed to {}:{}'.format(aspect_x, aspect_y))
+    try:
+        aspect_x = attrs['TOOLN_ImageAspectX']
+        aspect_y = attrs['TOOLN_ImageAspectY']
+    except KeyError:
+        print('To set pixel aspect ratio, add the Loader to the viewer')
+        aspect_x = aspect_y = None
+    if aspect_x and aspect_y:
+        comp.SetPrefs({'Comp.FrameFormat.AspectX': aspect_x, 'Comp.FrameFormat.AspectY': aspect_y})
+        print('Comp aspect is changed to {}:{}'.format(aspect_x, aspect_y))
 
     comp.SetPrefs({'Comp.FrameFormat.Name': 'Set by Script'})
 
