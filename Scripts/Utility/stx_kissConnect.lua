06d3c85bfb023ad2f8ce57d6bd9f496958e6a2a1
Alexey Bogomolov <abogomolov@mac.com>
2019-04-25 13:48:59 +0300

    refactoring, add check tool function

--- Scripts/Utility/stx_kissConnect.lua
+++ Scripts/Utility/stx_kissConnect.lua
@@ -1,42 +1,50 @@
 --stx_kissConnect
 --v1.2
--- disabled cmp locking, caused some issues
 
--- lock cmp
--- cmp:Lock()
-
--- get selection 
-cmp = fu:GetCurrentComp()
--- selectedNodes = cmp:GetToolList(true)
-selectedNodes = cmp:GetToolList(true)
-allNodes = cmp:GetToolList(false)
-
-flow = cmp.CurrentFrame.FlowView
-data = {}
-
-selX, selY = flow:GetPos(selectedNodes[1])
-
-distances = {}
-for i, n in pairs(allNodes) do
-	--print(flow:GetPos(allNodes[i]))
-	tempX, tempY = flow:GetPos(n)
-    if tempX and tempY then
-        local tempDist = math.sqrt((selX-tempX)*(selX-tempX)+(selY-tempY)*(selY-tempY))
-        table.insert(distances,{n, tempDist})
+function get_tool()
+    active = comp.ActiveTool
+    if not active then
+        selected_node = comp:GetToolList(true)[1]
+        if not selected_node then
+            print('select or activate node')
+            return None
+        end
+        return selected_node
     end
-	--print(tempDist)
+    return active
 end
 
-table.sort(distances, function(b, a) return a[2] > b[2] end)
 
--- for k,v in ipairs(distances) do
---     print(v[1].Name, ' == ', v[2])
--- end
+function main(tool)
+    cmp = fu:GetCurrentComp()
+    allNodes = cmp:GetToolList(false)
+    flow = cmp.CurrentFrame.FlowView
+    data = {}
+    selX, selY = flow:GetPos(tool)
+    distances = {}
 
-selectedNodes[1]:ConnectInput("Input", distances[2][1])
-selectedNodes[1]:ConnectInput("Background", distances[2][1])
-selectedNodes[1]:ConnectInput("MaterialInput", distances[2][1])
-selectedNodes[1]:ConnectInput("SceneInput", distances[2][1])
-selectedNodes[1]:ConnectInput("SceneInput1", distances[2][1])
+    for i, n in pairs(allNodes) do
+        tempX, tempY = flow:GetPos(n)
+        if tempX and tempY then
+            local tempDist = math.sqrt((selX-tempX)*(selX-tempX)+(selY-tempY)*(selY-tempY))
+            table.insert(distances,{n, tempDist})
+        end
+    end
 
---cmp:Unlock()
+    table.sort(distances, function(b, a) return a[2] > b[2] end)
+
+    -- for k,v in ipairs(distances) do
+    --     print(v[1].Name, ' == ', v[2])
+    -- end
+
+    tool:ConnectInput("Input", distances[2][1])
+    tool:ConnectInput("Background", distances[2][1])
+    tool:ConnectInput("MaterialInput", distances[2][1])
+    tool:ConnectInput("SceneInput", distances[2][1])
+    tool:ConnectInput("SceneInput1", distances[2][1])
+end
+
+tool = get_tool()
+if tool then
+   main(tool)
+end
