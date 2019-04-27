f6f81f79616e0ab60bb3287f571acfd864d94eae
Alex Bogomolov <mail@abogomolov.com>
2019-04-28 00:18:22 +0300

    delete file

--- Scripts/Tool/SetCompSize.lua
+++
@@ -1,6 +0,0 @@
-local width = tool:GetAttrs('TOOLI_ImageWidth')
-local height = tool:GetAttrs('TOOLI_ImageHeight')
-
-comp:SetPrefs({['Comp.FrameFormat.Width'] = width,  ['Comp.FrameFormat.Height'] = height})
-
-print('Comp size is adjusted to '.. width ..'x'.. height)
