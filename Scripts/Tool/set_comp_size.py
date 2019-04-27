96fc502e06557c1e8e357c0047a6b372f4905de2
Alex Bogomolov <mail@abogomolov.com>
2019-04-27 16:04:53 +0300

    clearify messages

--- Scripts/Tool/set_comp_size.py
+++ Scripts/Tool/set_comp_size.py
@@ -12,14 +12,14 @@ def main(tool):
         aspect_x = attrs['TOOLN_ImageAspectX']
         aspect_y = attrs['TOOLN_ImageAspectY']
     except KeyError:
-        print('To set pixel aspect ratio, add the Loader to the viewer')
+        print('To set defaut pixel aspect ratio, add the Loader to the viewer')
         aspect_x = aspect_y = None
 
     if aspect_x and aspect_y:
         comp.SetPrefs({'Comp.FrameFormat.AspectX': aspect_x,
                        'Comp.FrameFormat.AspectY': aspect_y})
 
-        print('Comp aspect is set to {}:{}'.format(aspect_x, aspect_y))
+        print('Comp pixel aspect is set to {}:{}'.format(aspect_x, aspect_y))
 
     comp.SetPrefs({'Comp.FrameFormat.Name': 'Set by Script'})
 
