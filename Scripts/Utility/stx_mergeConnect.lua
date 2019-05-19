b663d558b89384bb95cf59ac1f056569b50b4625
Alex Bogomolov <mail@abogomolov.com>
2019-05-19 15:21:44 +0300

    disable change pos for merge tool

--- Scripts/Utility/stx_mergeConnect.lua
+++ Scripts/Utility/stx_mergeConnect.lua
@@ -16,10 +16,11 @@ else
     flow = comp.CurrentFrame.FlowView
     x1, y1 = flow:GetPos(selectedNodes[1])
     x2, y2 = flow:GetPos(selectedNodes[2])
+    -- mousepos = fu:GetMousePos() 
     -- creates the merge node, connects it and sets the position to be somewhere in between the two
     mg1 = comp:AddTool("Merge", -32768, -32768)
     mg1:ConnectInput("Foreground", selectedNodes[1])
     mg1:ConnectInput("Background", selectedNodes[2])
-    flow:SetPos(mg1, x1-((x1-x2)/2) ,y1-((y1-y2)/2))
+    -- flow:SetPos(mg1, x1-((x1-x2)/2) ,y1-((y1-y2)/2))
 end
 
