41331653c4592eb23e95141ec58209734eb496e3
Alexey Bogomolov <abogomolov@mac.com>
2019-05-21 22:40:47 +0300

    refactor upstream scripts

--- Scripts/Tool/sel_backward.py
+++ Scripts/Tool/sel_backward.py
@@ -5,12 +5,13 @@ valid_types = ['Image', 'Particles', 'Mask', 'DataType3D']
 def recurse_select_backward(t):
     # if the current tool has image inputs, select them, otherwise pass
     flow.Select(t)
-    for inp in t.GetInputList().values():
-        if inp.GetAttrs()['INPS_DataType'] in valid_types:
-            next_out = inp.GetConnectedOutput()
-            if next_out:
-                recurse_select_backward(next_out.GetTool())
-        pass
+    inp_list = (inp for inp in t.GetInputList().values() 
+            if inp.GetAttrs()['INPS_DataType'] in valid_types)
+    for inp in inp_list:
+        next_out = inp.GetConnectedOutput()
+        if next_out:
+            tool = next_out.GetTool()
+            recurse_select_backward(tool)
 
 
 flow.Select()
--- Scripts/Tool/sel_forward.py
+++ Scripts/Tool/sel_forward.py
@@ -1,7 +1,8 @@
 flow = comp.CurrentFrame.FlowView
 valid_types = ['Image', 'Number', 'Point', 'Gradient', 'Text', 'Mask', 'DataType3D']
 
-tool = comp.ActiveTool
+if not tool:
+    tool = comp.ActiveTool
 
 
 def recurse_select_forward(t):
@@ -14,6 +15,7 @@ def recurse_select_forward(t):
             # print(inp.GetAttrs()['INPS_DataType'])
             if inp.GetAttrs()['INPS_DataType'] in valid_types:
                 recurse_select_forward(inp.GetTool())
+        # passes if the node is loaded to the viewer (it has empty key)
         except KeyError:
             pass
 
