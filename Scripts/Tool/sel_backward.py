cbeee515062cb780a4cd8b2dbab11bdb0d3b2efb
Alex Bogomolov <mail@abogomolov.com>
2019-05-24 18:58:32 +0300

    update ToolbarUI

--- Scripts/Tool/sel_backward.py
+++ Scripts/Tool/sel_backward.py
@@ -5,7 +5,7 @@ valid_types = ['Image', 'Particles', 'Mask', 'DataType3D']
 def recurse_select_backward(t):
     # if the current tool has image inputs, select them, otherwise pass
     flow.Select(t)
-    inp_list = (inp for inp in t.GetInputList().values() 
+    inp_list = (inp for inp in t.GetInputList().values()
             if inp.GetAttrs()['INPS_DataType'] in valid_types)
     for inp in inp_list:
         next_out = inp.GetConnectedOutput()
