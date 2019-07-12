1d9685276d7e1f546906964a938bd52a83ae8149
Alexey Bogomolov <abogomolov@mac.com>
2019-07-12 12:57:14 +0300

    update tools, delete dev stuff

--- Scripts/Tool/toolName.py
+++ Scripts/Tool/toolName.py
@@ -1,6 +1,7 @@
-# tool script to get tool's ID. Useful if you use a ForSlash script a lot,
-# and don't want to keep in memory, that Resize tool is actually a
-# 'BetterResize', and MaskPaint is actually a 'PaintMask'.
+# tool script to get tool's ID. Useful if you do expressions
+# or use a Reactor 'Slash For' script a lot
+# and you don't want to keep in memory, that Resize tool is actually a 'BetterResize',
+# and MaskPaint is actually a 'PaintMask'.
 
 import os
 import platform
@@ -9,4 +10,5 @@ tool_name = tool.ID
 
 if platform.system() in ["Darwin", "Linux"]:
     os.system("echo '{}' | tr -d '\n' | pbcopy".format(tool_name))
-print(tool.ID)
+
+print(tool_name)
