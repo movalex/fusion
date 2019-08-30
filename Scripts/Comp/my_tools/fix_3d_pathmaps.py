dda16ea617144ac9d5faac0c1c856c0897f45b6c
Alexey Bogomolov <mail@abogomolov.com>
2019-08-30 12:36:50 +0300

    remove commented code

--- Scripts/Comp/my_tools/fix_3d_pathmaps.py
+++ Scripts/Comp/my_tools/fix_3d_pathmaps.py
@@ -9,14 +9,3 @@ for fbx in all_fbx.values():
 for abc in all_abc.values():
     path = abc.Filename[1]
     abc.Filename[1] = comp.ReverseMapPath(path)
-
-# for tool in all_tools.values():
-#     if tool.ID == "SurfaceFBXMesh":
-#         path = tool.ImportFile[1]
-#         tool.ImportFile[1] = comp.ReverseMapPath(path)
-#     elif tool.ID == "SurfaceAlembicMesh":
-#         path = tool.Filename[1]
-#         tool.Filename[1] = comp.ReverseMapPath(path)
-#     else:
-#         pass
-
