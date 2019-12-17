8e6c246bc40e46acf0f33b862242c12a7f61dc68
Alex Bogomolov <mail@abogomolov.com>
2019-12-17 11:49:03 +0300

    move files

--- Scripts/Comp/my_tools/text_host.py
+++ Scripts/Comp/my_tools/text_host.py
@@ -1,11 +1,7 @@
 import socket
-try:
-    tool = comp.ActiveTool()
-    host = socket.gethostname()
-    text_host = comp.AddTool('TextPlus', -32768, -32768)
-    text_host.SetAttrs({'TOOLS_Name': 'computer_host'})
-    text_host.StyledText = host
-    text_host.Size = .03
-    text_host.Center = {1: .1, 2: .1, 3: 0}
-except TypeError:
-    print('Select tool')
+host = socket.gethostname()
+text_host = comp.AddTool('TextPlus', -32768, -32768)
+text_host.SetAttrs({'TOOLS_Name': 'computer_host'})
+text_host.StyledText = host
+text_host.Size = .03
+text_host.Center = {1: .1, 2: .1, 3: 0}
