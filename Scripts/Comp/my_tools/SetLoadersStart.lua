c6e915132ac91b2201b54276030b24307871ce95
Alex Bogomolov <mail@abogomolov.com>
2020-01-15 00:24:58 +0300

    add print logs

--- Scripts/Comp/my_tools/SetLoadersStart.lua
+++ Scripts/Comp/my_tools/SetLoadersStart.lua
@@ -1,6 +1,8 @@
+START_FRAME = 1
 comp:StartUndo('set loader start')
 for i, tool in ipairs(comp:GetToolList(true), "Loader") do
-    bmd.MoveClip(tool, comp:GetAttrs().COMPN_GlobalStart, 1)
+    print(tool.Name .. " " .. "global start is set to " .. START_FRAME)
+    bmd.MoveClip(tool, comp:GetAttrs().COMPN_GlobalStart, START_FRAME)
 end
 comp:EndUndo()
 
