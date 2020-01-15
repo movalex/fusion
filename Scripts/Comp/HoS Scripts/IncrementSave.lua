dbacde45342df8c2c51a3719668d022e1452e88c
abogomolov <mail@abogomolov.com>
2020-01-15 20:49:24 +0300

    wip incremental save macos compatibility

--- Scripts/Comp/HoS Scripts/IncrementSave.lua
+++ Scripts/Comp/HoS Scripts/IncrementSave.lua
@@ -4,6 +4,7 @@
   by S.Neve / House of Secrets
 
   some small updates from the inital release:
+  v1.4 fix macOs compatibility WIP
   v1.3 Fusion 9/16 support
   v1.2 changes:
     - Changed the way the postfix increment number for the incremental backup comp
@@ -29,45 +30,41 @@ function findpattern(text, pattern, start)
 	return string.sub(text, string.find(text, pattern, start))
 end
 
-if not composition then
-    composition = fu:GetCurrentComp()
+if not comp then
+    comp = fu:GetCurrentComp()
 end
 
 
-fa = composition:GetAttrs()
+fa = comp:GetAttrs()
 if fa.COMPS_FileName == "" then
-	Save()
+	comp:Save()
 else
 	pf = bmd.parseFilename(comp:MapPath(fa.COMPS_FileName))
 
 	if not direxists(pf.Path .. "incrementalSave") then
 		print("creating dir : " .. pf.Path .. "incrementalSave")
-		os.execute("mkdir \"" .. pf.Path .. "incrementalSave\"")
-	else
-		--print("dir exists")
+		os.execute("mkdir " .. pf.Path .. "incrementalSave\\".. pf.Name .. pf.Extension)
 	end
 
-	if not direxists( pf.Path .. "incrementalSave\\" .. pf.Name .. pf.Extension ) then
-		print("creating dir : " .. pf.Path .. "incrementalSave\\" .. pf.Name .. pf.Extension)
-		os.execute("mkdir \"" .. pf.Path .. "incrementalSave\\" .. pf.Name .. pf.Extension .. "\"")
-	else
-		--print("dir exists")
-	end
+    if not direxists(pf.Path .. "incrementalSave\\" .. pf.Name .. pf.Extension) then
+        print("creating dir : " .. pf.Path .. "incrementalSave\\" .. pf.Name .. pf.Extension)
+        os.execute("mkdir \"" .. pf.Path .. "incrementalSave\\" .. pf.Name .. pf.Extension .. "\"")
+    end
 
 	-- search inc saves
 	path = pf.Path .. "incrementalSave\\" .. pf.Name .. pf.Extension .. "\\*.comp"
-	dir = readdir(path)
+	dir = bmd.readdir(path)
 	num = table.getn(dir)
 	currentVersion = 0
 
 	for i = 1,num do
 		if not dir[i].IsDir then
-			fileExtension = string.gsub(dir[i].Name, "[.][^.]*$", "")
-			--fileNumberString = string.gsub( fileExtension, string.gsub( fileExtension, "[^.][0-9]+$", ""), "")
+			fileExtension = string.gsub(dir[i].Name, "[.][^.]+$", "")
 			fileNumberString = string.sub(fileExtension, string.find(fileExtension, "(%d+)$", 0))
 			fileNumber = tonumber(fileNumberString)
-			--print(fileNumberString)
-			--print(fileNumber)
+            -- print(fileExtension)
+            -- print(fileNumber)
+            -- print(fileNumberString)
 			if currentVersion < fileNumber then
 				currentVersion = fileNumber
 			end
@@ -76,8 +73,8 @@ else
 	currentVersionString = "000" .. tostring(currentVersion + 1)
 	currentVersionString = string.sub(currentVersionString, string.len(currentVersionString) - 3 , string.len(currentVersionString))
 
+    src =  pf.Path .. pf.Name .. pf.Extension
 	dest =  pf.Path .. "incrementalSave\\" .. pf.Name .. pf.Extension .. "\\" .. pf.Name .. "." .. currentVersionString .. ".comp"
-	src =  pf.Path .. pf.Name .. pf.Extension
 	os.rename(src, dest) -- this seems to work in Lightwave, and does the same in Fusion, much cleaner isn't it?
 	comp:Save(src)
 end
