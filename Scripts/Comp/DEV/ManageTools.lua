481e531bf2ae3d947746a9941f5b250ad7438bcf
Alex Bogomolov <mail@abogomolov.com>
2020-04-11 18:15:12 +0300

    add About window

--- Scripts/Comp/DEV/ManageTools.lua
+++ Scripts/Comp/DEV/ManageTools.lua
@@ -1,9 +1,18 @@
+_author = "Alexey Bogomolov <mail@abogomolov.com>"
+_date = "2020-04-11"
+_VERSION = "1.0"
+
 local ui = fu.UIManager
 local disp = bmd.UIDispatcher(ui)
 local width,height = 270,160
+
 if not comp then
     comp = fu:GetCurrentComp()
 end
+
+print(string.format("[Manage Tools] - v%s %s ", _VERSION, _date))
+print(string.format("[Created By] %s\n\n", _author))
+
 checked_state = comp:GetData('MT.use_comments')
 flow = comp.CurrentFrame.FlowView
 
@@ -12,7 +21,6 @@ if fu.Version == 16.2 and FuPLATFORM_MAC == true then
     print('JIT disabled for v16.2 on macOS')
     jit.off()
 end
-
 
 function fetch_data(comp)
     last_tool = comp:GetData('MT.tool_id') or get_selected_tool()
@@ -47,11 +55,77 @@ function print_label()
     end
 end
 
--- local x = fu:GetMousePos()[1]
--- local y = fu:GetMousePos()[2]
--- if y < 90 then
---     y = 130
--- end
+app:AddConfig("Manage",
+{
+    Target  {ID = "Manage"},
+    Hotkeys {Target = "Manage",
+             Defaults = true,
+             ESCAPE = "Execute{cmd = [[app.UIManager:QueueEvent(obj, 'Close', {})]]}" }
+})
+
+function AboutWindow()
+	local URL = 'https://abogomolov.com'
+
+	local width, height = 520, 250
+	aboutWin = disp:AddWindow({
+		ID = "AboutWin",
+		WindowTitle = 'About Dialog',
+		WindowFlags = {Window = true, WindowStaysOnTopHint = true,},
+		Geometry = {200, 200, width, height},
+
+		ui:VGroup{
+			ID = 'root',
+
+			-- Add your GUI elements here:
+			ui:TextEdit{ID = 'AboutText', ReadOnly = true, Alignment = {AlignHCenter = true, AlignTop = true}, HTML = '<h1>Tool Manager</h1>\n<p>Version '.._VERSION..' - '.. _date..' </p>\n<p>Use this script to quickly enable/disable multiple tools. Select the tool and button to disable tools of the same kind. You can set specific tools to manage. Select tool and set comment with "Set" button. If the checkbox \'With Comment\' is enabled, only tools with comments will be affected. You can also toggle enable/disable for all tools with the same comment.</p> \n<p>Copyright &copy; 2020 Alexey Bogomolov (MIT License)</p>',},
+
+			ui:VGroup{
+				Weight = 0,
+
+                ui:Label{
+					ID = "EMAIL",
+					Text = 'Email: <a href="' .. 'mailto:mail@abogomolov.com' .. '">' .. 'mail@abogomolov.com' .. '</a>',
+					Alignment = {
+						AlignHCenter = true,
+						AlignTop = true,
+					},
+					WordWrap = true,
+					OpenExternalLinks = true,
+				},
+                ui:Label{
+					ID = "Donate",
+					Text = 'Donate: <a href="' .. 'https://paypal.me/aabogomolov/5usd' .. '">' .. 'https://paypal.me/aabogomolov/' .. '</a>',
+					Alignment = {
+						AlignHCenter = true,
+						AlignTop = true,
+					},
+					WordWrap = true,
+					OpenExternalLinks = true,
+				},
+
+			},
+		},
+	})
+	local itm = aboutWin:GetItems()
+
+	-- The window was closed
+	function aboutWin.On.AboutWin.Close(ev)
+		disp:ExitLoop()
+        win:Show()
+	end
+	aboutWin:Show()
+	disp:RunLoop()
+	aboutWin:Hide()
+
+	-- return aboutWin,aboutWin:GetItems()
+end
+mainWin = ui:FindWindow("Manage")
+if mainWin then
+    mainWin:Raise()
+    mainWin:ActivateWindow()
+    return
+end
+
 win = disp:AddWindow({
   ID = 'Manage',
   TargetID = 'Manage',
@@ -68,9 +142,8 @@ win = disp:AddWindow({
     },
     ui:HGroup {
         ui:Label{ID = 'Label', Weight = .6, Text = print_label()},
-
-        -- ui:Button{ID = 'Find', Text = 'Find', Weight=0.1,MinimumSize = {30,16}, 
-        -- Alignment = {AlignRight = true}},
+        ui:Button{ID = 'Info', Text = 'info', Weight=0.1,MinimumSize = {30,16}, 
+        Alignment = {AlignRight = true}},
             },
     ui:HGroup {
 
@@ -89,7 +162,6 @@ win = disp:AddWindow({
 })
 
 itm = win:GetItems()
-
 
 function win.On.Select.Clicked(ev)
     local comp = fu:GetCurrentComp()
@@ -131,7 +203,6 @@ function win.On.SetComment.Clicked(ev)
     comp:SetData('MT.comment', new_comment)
 end
 
-
 function win.On.Toggle.Clicked(ev)
     local comp = fu:GetCurrentComp()
     itm['checkbox'].Checked = true
@@ -153,20 +224,17 @@ function win.On.Toggle.Clicked(ev)
     end
 end
 
-
 function win.On.Manage.Close(ev)
     local comp = fu:GetCurrentComp()
     comp:SetData('MT.use_comments', itm['checkbox'].Checked)
     disp:ExitLoop()
 end
 
-
 function win.On.Line.ReturnPressed(ev)
     local comp = fu:GetCurrentComp()
     comp:SetData('MT.comment', itm['Line'].Text)
     print('now searching for comment "'..comp:GetData('MT.comment')..'"')
 end
-
 
 function doPassThrough(operation, report)
     local comp = fu:GetCurrentComp()
@@ -213,23 +281,20 @@ function doPassThrough(operation, report)
     end
 end
 
-
 function win.On.Disable.Clicked(ev)
     doPassThrough(true, 'disabled ')
 end
-
 
 function win.On.Enable.Clicked(ev)
     doPassThrough(false, 'enabled ')
 end
 
-app:AddConfig("Manage",
-{
-    Target  {ID = "Manage"},
-    Hotkeys {Target = "Manage",
-             Defaults = true,
-             ESCAPE = "Execute{cmd = [[app.UIManager:QueueEvent(obj, 'Close', {})]]}" }
-})
+function win.On.Info.Clicked(ev)
+    win:Hide()
+    AboutWindow()
+end
+
+
 
 win:Show()
 disp:RunLoop()
