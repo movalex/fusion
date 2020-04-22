a65cf0db90e119145d83810614ca20800b187a89
Alex Bogomolov <mail@abogomolov.com>
2020-04-22 16:19:27 +0300

    add optional save location

--- Scripts/Comp/HoS Scripts/IncrementSave.lua
+++ Scripts/Comp/HoS Scripts/IncrementSave.lua
@@ -1,11 +1,12 @@
 --[[
-  hos_incrementalSave
+  incrementalSave
 
-  by S.Neve / House of Secrets
 
   some small updates from the inital release:
-  v1.4:
-  - add MacOS compatibility by Alex Bogomolov -- 2019/01/16
+  v1.4: by Alex Bogomolov 
+  - add MacOS compatibility -- 2019/01/16
+  - add optional save location variable -- 2020/04/22
+        set $Increment_Save_Path variable for custom save folder 
   v1.3:
   - add Fusion 9/16 support
   v1.2 changes:
@@ -20,16 +21,13 @@
   - encapsulated the DOS file names in quotation marks to make creation in and
     of directories with spaces possible.
   v1.0 changes:
-  - Initial release
+  - Initial release by S.Neve / House of Secrets
 ]]--
 	
-function findpattern(text, pattern, start)
-	return string.sub(text, string.find(text, pattern, start))
-end
-
 if not comp then
     comp = fu:GetCurrentComp()
 end
+
 mkdir_recursive = ''
 split_path = '\\'
 platform = (FuPLATFORM_WINDOWS and "Windows") or (FuPLATFORM_MAC and "Mac") or (FuPLATFORM_LINUX and "Linux")
@@ -37,25 +35,43 @@ if platform == 'Mac' or platform == 'Linux' then
     mkdir_recursive = '-p '
     split_path = '/'
 end
+
+function getSavePath()
+     local pathEnv = comp:MapPath(os.getenv('Increment_Save_Path'))
+     -- print('env: ', pathEnv)
+     if pathEnv then
+         if not direxists(pathEnv) then
+             print('no dir found')
+             os.execute('mkdir' .. mkdir_recursive .. pathEnv)
+         end
+         return pathEnv .. comp:GetAttrs().COMPS_Name .. '.comp'
+     else
+         return comp:MapPath(comp:GetAttrs().COMPS_FileName)
+     end
+end
+
 fa = comp:GetAttrs()
 if fa.COMPS_FileName == "" then
 	comp:Save()
 else
-	pf = bmd.parseFilename(comp:MapPath(fa.COMPS_FileName))
+    compPath = getSavePath() 
+    -- print("get path: ", compPath)
+    pf = bmd.parseFilename(compPath)
+    -- dump(pf)
 
 	if not direxists(pf.Path .. "incrementalSave") then
-		print("creating dir : " .. pf.Path .. "incrementalSave" .. split_path.. pf.Name)
+		print("creating dir: " .. pf.Path .. "incrementalSave" .. split_path.. pf.Name)
 		os.execute("mkdir ".. mkdir_recursive .. pf.Path .. "incrementalSave" .. split_path.. pf.Name)
     end
 
     if not direxists(pf.Path .. "incrementalSave".. split_path .. pf.Name) then
-        print("creating dir : " .. pf.Path .. "incrementalSave" .. split_path .. pf.Name)
+        print("creating dir: " .. pf.Path .. "incrementalSave" .. split_path .. pf.Name)
 		os.execute("mkdir ".. mkdir_recursive .. pf.Path .. "incrementalSave" .. split_path .. pf.Name)
     end
 
 	-- search inc saves
-	path = pf.Path .. "incrementalSave" .. split_path .. pf.Name .. split_path .."*.comp"
-	dir = bmd.readdir(path)
+	pathSearch = pf.Path .. "incrementalSave" .. split_path .. pf.Name .. split_path .."*.comp"
+	dir = bmd.readdir(pathSearch)
 	num = table.getn(dir)
 	currentVersion = 0
 
@@ -72,8 +88,11 @@ else
 	currentVersionString = "000" .. tostring(currentVersion + 1)
 	currentVersionString = string.sub(currentVersionString, string.len(currentVersionString) - 3 , string.len(currentVersionString))
 
-    src =  pf.Path .. pf.Name .. pf.Extension
+    src =  comp:GetAttrs().COMPS_FileName
+    -- print('src: ',  src)
+
 	dest =  pf.Path .. "incrementalSave" .. split_path .. pf.Name .. split_path .. pf.Name .. "." .. currentVersionString .. ".comp"
-    os.rename(src, dest) -- this seems to work in Lightwave, and does the same in Fusion, much cleaner isn't it?
+    -- print('dest: ', dest)
+    os.rename(src, dest)
 	comp:Save(src)
 end
