42eb5ff8cb8b3e3478dbb8182cb74a9277a2f29c
Alex Bogomolov <mail@abogomolov.com>
2020-05-08 00:00:14 +0300

    working script version

--- Scripts/Comp/DEV/IncrementSave.py
+++ Scripts/Comp/DEV/IncrementSave.py
@@ -2,90 +2,62 @@
   Incremental Save script
   by Alex Bogomolov
 
-  Based on IncrementalSave lua script by S.Neve / House of Secrets
+  Based on IncrementalSave.lua script by S.Neve / House of Secrets
 """
-import platform
 import os
+import platform
+import re
 from pathlib import Path
 
 comp = fu.GetCurrentComp()
-
-# if platform.system() == "Windows":
-#     pass
-# elif platform.system() in ["Darwin", "Linux"]:
-#     pass
-
 PATHENV = comp.MapPath(os.getenv("INCREMENT_SAVE_PATH"))
-
 comp_attrs = comp.GetAttrs()
 
-def get_save_path(path_env):
+
+def get_save_path(path_env=None):
     if path_env:
         comp_path = Path(comp_attrs["COMPS_FileName"])
         path_env = Path(path_env)
-        print(f"env: {path_env}")
+        print(f"environment path found: {path_env}")
         if not path_env.exists():
             print("no save directory found, creating now")
-            path_env.mkdir(exist_ok=True)
+            path_env.mkdir(parents=True, exist_ok=True)
         return path_env / comp_path.stem
-    else:
-        rootSaveFolder = "IncrementSave"
-        comp_path = Path(comp.MapPath(comp.GetAttrs()["COMPS_FileName"]))
-        return comp_path.parent / rootSaveFolder / comp_path.stem
+    rootSaveFolder = "IncrementSave"
+    comp_path = Path(comp.MapPath(comp.GetAttrs()["COMPS_FileName"]))
+    return comp_path.parent / rootSaveFolder / comp_path.stem
+
+
+def get_comp_files(path):
+    comps = []
+    for file in path.iterdir():
+        if file.suffix == ".comp":
+            comps.append(file)
+    return comps
+
+
+def increment_comp():
+    number = 0
+    source_file = Path(comp_attrs["COMPS_FileName"])
+    comp_name = Path(comp_attrs["COMPS_Name"])
+    print(f'source: {source_file}')
+    save_path = get_save_path(PATHENV)
+    if not save_path.exists():
+        save_path.mkdir(parents=True)
+    comp_list = get_comp_files(save_path)
+    if len(comp_list) > 0:
+        for file in comp_list:
+            num = re.findall("(\d{4})", str(file))[0]
+            if int(num) > number:
+                number = int(num)
+    number += 1
+    dest_file = save_path / comp_name.with_suffix(f'.{number:04}.comp')
+    print(f'destination: {dest_file}')
+    source_file.rename(str(dest_file))
+    comp.Save(source_file)
+
 
 if __name__ == "__main__":
     if comp_attrs["COMPS_FileName"] == "":
         comp.Save()
-    path = get_save_path(PATHENV)
-    print(path)
-
-
-def main():
-    comp_path = get_save_path(PATHENV)
-    pf = bmd.parseFilename(comp_path)
-    if not bmd.direxists(pf.Path + rootSaveFolder):
-        print("creating dir: " + pf.Path + rootSaveFolder + split_path + pf.Name)
-        os.execute(
-            "mkdir " + mkdir_recursive + pf.Path + rootSaveFolder + split_path + pf.Name
-        )
-    if not bmd.direxists(pf.Path + rootSaveFolder + split_path + pf.Name):
-        print("creating dir: " + pf.Path + rootSaveFolder + split_path + pf.Name)
-        os.execute(
-            "mkdir " + mkdir_recursive + pf.Path + rootSaveFolder + split_path + pf.Name
-        )
-        # search inc saves
-        pathSearch = (
-            pf.Path + rootSaveFolder + split_path + pf.Name + split_path + "*.comp"
-        )
-        dir = bmd.readdir(pathSearch)
-        num = len(dir)
-        currentVersion = 0
-        for i in range(num):
-            if not dir.is_dir():
-                fileExtension = string.gsub(dir[i].Name, "[.][^.]+$", "")
-                fileNumberString = string.sub(
-                    fileExtension, string.find(fileExtension, "(%d+)$", 0)
-                )
-                fileNumber = tonumber(fileNumberString)
-                if currentVersion < fileNumber:
-                    currentVersion = fileNumber
-        currentVersionString = "000" + tostring(currentVersion + 1)
-        currentVersionString = string.sub(
-            currentVersionString,
-            string.len(currentVersionString) - 3,
-            string.len(currentVersionString),
-        )
-    src = comp.GetAttrs().COMPS_FileName
-    dest = (
-        pf.Path
-        + rootSaveFolder
-        + split_path
-        + pf.Name
-        + split_path
-        + pf.Name
-        + "."
-        + currentVersionString
-        + ".comp"
-    )
-    os.rename(src, dest)
-    comp.Save(src)
+    increment_comp()
