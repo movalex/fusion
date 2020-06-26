7b3168e9348fd9bb6e2a8e394a2f2035ed334227
Alex Bogomolov <mail@abogomolov.com>
2020-06-26 09:09:57 +0300

    update description, add Python2 support

--- Scripts/Comp/DEV/IncrementSave.py
+++ Scripts/Comp/DEV/IncrementSave.py
@@ -2,17 +2,17 @@
   Increment Save script
   by Alexey Bogomolov
   
-  Ths script is based on IncrementalSave.lua script written by S.Neve / House of Secrets
-    NOTE: This version of the script works with Python3 only, since Python2 is deprecated
-    If you are still using Python2 feel free to alter all f-strings to .format() or use IncrementSave.lua
-    However, this version has some advantages over the Lua one:
+    Ths script is based on IncrementalSave.lua script written by S.Neve / House of Secrets
+    Python2 and 3 are supported, but usage of Python3 is encouraged
+    This version has some advantages over the Lua one:
         - it will work if the comp path has some non-ascii characters
         - save folder will look the same on Mac and PC - without '.comp' part 
-              (comp.GetAtttr()["COMPS_Filename"] works differently on windows and mac)
-        - In my opinion it is faster and easier to read and maintain
+              (Sic: comp.GetAtttr()["COMPS_Filename"] works differently on Windows and Mac!)
+        - it is  easier to read and maintain
     MIT License:  https://mit-license.org/
     Copyright 2020 Alexey Bogomolov
-    Version: 1.0
+    v.1.0 - 2020/06/26:
+        * initial release
     email: mail@abogomolov.com
     donations: https://paypal.me/aabogomolov
 """
@@ -32,7 +32,7 @@ def get_save_path(path_env=None):
     if path_env:
         path_env = Path(path_env)
         comp_path = Path(comp_attrs["COMPS_FileName"])
-        print(f"environment path found: {path_env}")
+        print("environment path found: {}".format(path_env))
         if not path_env.exists():
             # print("no save directory found, creating now")
             path_env.mkdir(parents=True, exist_ok=True)
@@ -57,19 +57,15 @@ def get_increment_number(path):
 
 
 def increment_comp():
-    if not (sys.version_info.major == 3 and sys.version_info.minor >= 6):
-        print("this script requires Python version >= 3.6")
-        return
     source_file = Path(comp_attrs["COMPS_FileName"])
     comp_name = Path(comp_attrs["COMPS_Name"])
-    # print(f"source: {source_file}")
     save_path = get_save_path(PATHENV)
     if not save_path.exists():
         save_path.mkdir(parents=True)
     number = get_increment_number(save_path)
     number += 1
-    dest_file = save_path / comp_name.with_suffix(f".{number:04}.comp")
-    print(f"save destination: {dest_file}")
+    dest_file = save_path / comp_name.with_suffix(".{:04}.comp".format(number))
+    print("save destination: {}".format(dest_file))
     try:
         source_file.rename(str(dest_file))
     except OSError:
