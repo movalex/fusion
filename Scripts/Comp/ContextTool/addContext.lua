89058291126491982098ccf0ac8d0af4c0e45165
Alexey Bogomolov <mail@abogomolov.com>
2020-10-23 12:45:12 +0300

    update context tool

--- Scripts/Comp/ContextTool/addContext.lua
+++ Scripts/Comp/ContextTool/addContext.lua
@@ -1,12 +1,14 @@
 -- local CONTEXT = 1
+tool = comp.ActiveTool or comp:GetToolList(true)[1] 
 if not tool then
-    tool = comp.ActiveTool or comp:GetToolList(true)[1] 
+    print ('no tool selected')
+else
+    local getPreviousToolName = comp:GetData("ContextTool."..CONTEXT)
+    local prevTool = comp:FindTool(getPreviousToolName)
+    if prevTool and prevTool ~= tool then
+        prevTool.Comments[fu.TIME_UNDEFINED] = ""
+    end
+    comp:SetData("ContextTool."..CONTEXT, tool.Name)
+    print(tool.Name ..' is set as context view #'..CONTEXT)
+    tool.Comments = 'Context ' .. CONTEXT
 end
-local getToolName = comp:GetData("ContextTool."..CONTEXT)
-local prevTool = comp:FindTool(getToolName)
-if prevTool and prevTool ~= tool then
-    prevTool.Comments[fu.TIME_UNDEFINED] = ""
-end
-comp:SetData("ContextTool."..CONTEXT, tool.Name)
-print(tool.Name ..' is set as context view #'..CONTEXT)
-tool.Comments = 'Context ' .. CONTEXT
