ecf05b68f5b3510206b14fe03dd2ebf311784cf9
Alexey Bogomolov <mail@abogomolov.com>
2020-10-23 12:45:59 +0300

    move loaders to in point

--- Scripts/Tool/MoveLoaders.lua
+++ Scripts/Tool/MoveLoaders.lua
@@ -1,5 +1,3 @@
-newStart = comp:GetData("FrameHandles.offset") or 24
-
 function moveLoader(tool, startFrom)
     if tool.ID ~= "Loader" then
         print('this is not a Loader tool')
@@ -8,6 +6,13 @@ function moveLoader(tool, startFrom)
     comp:StartUndo('set loader start')
     bmd.MoveClip(tool, comp:GetAttrs().COMPN_GlobalStart, tonumber(startFrom))
     comp:EndUndo()
+end
+
+newStart = comp:GetData("FrameHandles.offset") or 24
+inPoint = comp:GetAttrs().COMPN_RenderStart
+
+if newStart < inPoint then
+    newStart = inPoint
 end
 
 moveLoader(tool, newStart)
