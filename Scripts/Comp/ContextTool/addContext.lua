7b2e080a7e5e4eafc1a524fd74cd51ac3d21f91b
Alexey Bogomolov <mail@abogomolov.com>
2020-10-26 04:30:54 +0300

    refactoring

--- Scripts/Comp/ContextTool/addContext.lua
+++ Scripts/Comp/ContextTool/addContext.lua
@@ -4,9 +4,9 @@ if not tool then
     print ('no tool selected')
 else
     local getPreviousToolName = comp:GetData("ContextTool."..CONTEXT)
-    local prevTool = comp:FindTool(getPreviousToolName)
-    if prevTool and prevTool ~= tool then
-        prevTool.Comments[fu.TIME_UNDEFINED] = ""
+    local previousTool = comp:FindTool(getPreviousToolName)
+    if previousTool and previousTool ~= tool then
+        previousTool.Comments[fu.TIME_UNDEFINED] = ""
     end
     comp:SetData("ContextTool."..CONTEXT, tool.Name)
     print(tool.Name ..' is set as context view #'..CONTEXT)
