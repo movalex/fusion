396bd89d8224162255ef8111344e4cc30b1eb142
Alexey Bogomolov <mail@abogomolov.com>
2020-11-09 11:00:06 +0300

    add description, refactoring

--- Scripts/Comp/DEV/IncrementSave.py
+++ Scripts/Comp/DEV/IncrementSave.py
@@ -2,15 +2,20 @@
   Increment Save script
   by Alex Bogomolov
   
-  Ths script is based on IncrementalSave.lua script written by S.Neve / House of Secrets
-    NOTE: This version of the script works with Python3 only, since Python2 is deprecated
-    If you are still using Python2 feel free to  use IncrementSave.lua
-    However this version has some advantages over the Lua one:
-        - it will work if the comp path has some non-ascii characters
-        - save folder will look the same on Mac and PC - without '.comp' part 
-              (Sic: comp.GetAtttr()["COMPS_Filename"] works differently on Windows and Mac!)
-        - it is  easier to read and maintain
-    MIT License:  https://mit-license.org/
+    The script logic is based on IncrementalSave script written by S.Neve / House of Secrets
+    NOTE: The script requires Python3. If you are still using Python2,
+    feel free to use IncrementSave.lua from this Reactor installation
+    However Python version of IncrementSave has some advantages over the Lua one:
+        - it will work correctly if the comp path has some non-ascii characters
+        - increment save folder will look the same on Mac and PC - without the '.comp' part 
+        - it is easier to read and maintain
+    USAGE:
+        - Run the script from Scripts/Comp/IncrementSave folder or use Alt+S shortcut
+        - The folder IncrementSave will be created, if it does not exists, in comp folder
+        - Inside IncrementSave folder there will be created a folder with a comp name, which contains the comp with
+          incremented version number in ...0001.comp format
+        - Set $INCREMENT_SAVE_PATH environment variable to have your increment saves in specific folder
+    MIT License: https://mit-license.org/
     Copyright 2020 Alex Bogomolov
     v.1.0 - 2020/06/26:
         * initial release
@@ -19,13 +24,12 @@
 """
 
 import os
-import platform
 import re
 import sys
 from pathlib import Path
 
 comp = fu.GetCurrentComp()
-PATHENV = comp.MapPath(os.getenv("INCREMENT_SAVE_PATH"))
+PATH_ENV = comp.MapPath(os.getenv("INCREMENT_SAVE_PATH"))
 comp_attrs = comp.GetAttrs()
 
 
@@ -38,7 +42,7 @@ def get_save_path(path_env=None):
             path_env.mkdir(parents=True, exist_ok=True)
         return path_env / comp_path.stem
     root_save_folder = "IncrementSave"
-    comp_path = Path(comp.MapPath(comp.GetAttrs()["COMPS_FileName"]))
+    comp_path = Path(comp.MapPath(comp_attrs["COMPS_FileName"]))
     return comp_path.parent / root_save_folder / comp_path.stem
 
 
@@ -50,7 +54,7 @@ def get_increment_number(path):
             comps.append(file)
     if len(comps) > 0:
         for file in comps:
-            num = re.findall("(\d{4}).comp$", str(file))[0]
+            num = re.findall(r"(\d{4}).comp$", str(file))[0]
             if int(num) > increment_number:
                 increment_number = int(num)
     return increment_number
@@ -62,15 +66,15 @@ def increment_comp():
         return
     source_file = Path(comp_attrs["COMPS_FileName"])
     comp_name = Path(comp_attrs["COMPS_Name"])
-    save_path = get_save_path(PATHENV)
+    save_path = get_save_path(PATH_ENV)
     if not save_path.exists():
         save_path.mkdir(parents=True)
     number = get_increment_number(save_path)
     number += 1
-    dest_file = save_path / comp_name.with_suffix(f".{number:04}.comp")
-    print(f"save destination: {dest_file}")
+    destination_file = save_path / comp_name.with_suffix(f".{number:04}.comp")
+    print(f"saved comp version: {destination_file.name}")
     try:
-        source_file.rename(str(dest_file))
+        source_file.rename(str(destination_file))
     except OSError:
         print("source file name is empty")
     comp.Save(source_file)
