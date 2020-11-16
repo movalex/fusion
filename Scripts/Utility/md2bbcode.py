aafbf73e6aa9407abc90e31c66804e05bc5d9e67
Alexey Bogomolov <mail@abogomolov.com>
2020-11-17 02:07:45 +0300

    update for STU, add md to html

--- Scripts/Utility/md2bbcode.py
+++ Scripts/Utility/md2bbcode.py
@@ -1,14 +1,20 @@
 import re
+import sys
+import os
+from markdown import markdown
+
 
 def markdown_to_bbcode(s):
     links = {}
     codes = []
     def gather_link(m):
-        links[m.group(1)]=m.group(2); return ""
+        links[m.group(1)]=m.group(2)
+        return ""
     def replace_link(m):
         return "[url=%s]%s[/url]" % (links[m.group(2) or m.group(1)], m.group(1))
     def gather_code(m):
-        codes.append(m.group(3)); return "[code=%d]" % len(codes)
+        codes.append(m.group(3))
+        return "[c=%d]" % len(codes)
     def replace_code(m):
         return "%s" % codes[int(m.group(1)) - 1]
     
@@ -18,14 +24,16 @@ def markdown_to_bbcode(s):
             s = re.sub(r"(`+)(\s*)(.*?)\2\1", gather_code, s)
             s = re.sub(r"\[(.*?)\]\[(.*?)\]", replace_link, s)
             s = re.sub(r"\[(.*?)\]\((.*?)\)", "[url=\\2]\\1[/url]", s)
-            s = re.sub(r"<(https?:\S+)>", "[url=\\1]\\1[/url]", s)
+            # s = re.sub(r"(https?:\S+)", "[url=\\1]\\1[/url]", s)
             s = re.sub(r"\B([*_]{2})\b(.+?)\1\B", "[b]\\2[/b]", s)
-            s = re.sub(r"\B([*_])\b(.+?)\1\B", "[i]\\2[/i]", s)
+            # s = re.sub(r"\B([_]{1})\b(.+?)\1\B", "[i]\\2[/i]", s)
             return p % s
         return inline
     
     s = re.sub(r"(?m)^\[(.*?)]:\s*(\S+).*$", gather_link, s)
     s = re.sub(r"(?m)^    (.*)$", "~[code]\\1[/code]", s)
+    s = re.sub(r"(?m)(`)(.*?)(`)", "[c]\\2[/c]", s)
+    s = re.sub(r"(?m)\b([*_]{1})(.*?)\1\b", "[i]\\2[/i]", s)
     s = re.sub(r"(?m)^(\S.*)\n=+\s*$", translate("~[size=200][b]%s[/b][/size]"), s)
     s = re.sub(r"(?m)^(\S.*)\n-+\s*$", translate("~[size=100][b]%s[/b][/size]"), s)
     s = re.sub(r"(?m)^#\s+(.*?)\s*#*$", translate("~[size=200][b]%s[/b][/size]"), s)
@@ -40,5 +48,23 @@ def markdown_to_bbcode(s):
     s = re.sub(r"\[/quote]\n\[quote]", "\n", s)
     s = re.sub(r"\[/list]\n\[list(=1)?]\n", "", s)
     s = re.sub(r"(?m)\[code=(\d+)]", replace_code, s)
-    
+
     return s
+
+
+def main():
+    file_path = sys.argv[1]
+    file_name, ext = os.path.splitext(file_path)
+    text = ""
+    with open(file_path, "r") as fp:
+        for line in fp:
+            text += line
+    output = markdown_to_bbcode(text)
+    with open(file_name + ".txt", "w") as out:
+        out.write(output)
+    with open(file_name + ".html", "w") as html:
+        html.write(markdown(text))
+    print("Done!")
+
+if __name__ == "__main__":
+    main()
