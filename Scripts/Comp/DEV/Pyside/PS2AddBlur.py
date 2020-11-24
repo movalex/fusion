63ccaff82ac26726ff9aa51033ef085a2b94e707
Alexey Bogomolov <mail@abogomolov.com>
2020-11-24 10:56:47 +0300

    update pyside scripts

--- Scripts/Comp/DEV/Pyside/PS2AddBlur.py
+++ Scripts/Comp/DEV/Pyside/PS2AddBlur.py
@@ -1,3 +1,4 @@
+import sys
 from PySide2.QtCore import (
     QSize,
     Qt,
@@ -10,13 +11,21 @@ from PySide2.QtWidgets import (
     QWidget,
 )
 
+try:
+    import BlackmagicFusion as bmd
+
+    fu = bmd.scriptapp("Fusion")
+except ImportError:
+    print("no Fusion app found")
+    sys.exit()
+
 
 class BlurClass(QMainWindow):
     def __init__(self, parent=None):
         QMainWindow.__init__(self, parent)
         self.setWindowTitle("Add Blur")
-        self.resize(250, 50)
-        self.setMaximumSize(QSize(300, 130))
+        self.resize(450, 50)
+        self.setMaximumSize(QSize(700, 130))
         central_widget = QWidget()
         self.setCentralWidget(central_widget)
         horizontal_layout = QHBoxLayout(central_widget)
@@ -33,12 +42,16 @@ class BlurClass(QMainWindow):
     @staticmethod
     def add_blur_node():
         comp = fu.GetCurrentComp()
-        blur = comp.AddTool("Blur")
+        blur = comp.AddTool("Blur", -32768, -32768)
         print(f"Node {blur.Name} added from PySide2 window!")
 
 
-if __name__ == '__main__':
-    app = QApplication([])
+if __name__ == "__main__":
+    # create QApplication if it doesnt exist
+    app = QApplication.instance()
+    if not app:
+        app = QApplication(sys.argv)
     w = BlurClass()
+    app.setAttribute(Qt.AA_DisableHighDpiScaling)
     w.show()
     app.exec_()
