a9fcfe8658594d3a58c117526cf094ca22c8985a
Alexey Bogomolov <mail@abogomolov.com>
2020-11-24 10:57:59 +0300

    properly switch contexts

--- Scripts/Comp/ContextTool/addContext.lua
+++ Scripts/Comp/ContextTool/addContext.lua
@@ -1,14 +1,49 @@
--- local CONTEXT = 1
+function has_key(tab, key)
+    for k,v in ipairs(tab) do
+        if k == key then
+            return true
+        end
+    end
+    return false
+end
+
 tool = comp.ActiveTool or comp:GetToolList(true)[1] 
+
 if not tool then
     print ('no tool selected')
-else
-    local getPreviousToolName = comp:GetData("ContextTool."..CONTEXT)
-    local previousTool = comp:FindTool(getPreviousToolName)
-    if previousTool and previousTool ~= tool then
-        previousTool.Comments[fu.TIME_UNDEFINED] = ""
-    end
-    comp:SetData("ContextTool."..CONTEXT, tool.Name)
-    print(tool.Name ..' is set as context view #'..CONTEXT)
-    tool.Comments = 'Context ' .. CONTEXT
+    return
 end
+
+data = comp:GetData("ContextTool.contexts")
+
+if not CONTEXT then
+    for i = 1, 10 do
+        if data and has_key(data, i) then
+            CONTEXT = i + 1
+        else
+            CONTEXT = 1
+        end
+    end
+end
+
+if data then
+    local dataToolName = data[CONTEXT]
+    local findToolFromData = comp:FindTool(dataToolName)
+    if findToolFromData and findToolFromData ~= tool then
+        findToolFromData.Comments[fu.TIME_UNDEFINED] = ""
+    end
+    for i, name in ipairs(data) do
+        if name == tool.Name and i ~= CONTEXT then
+            table.remove(data, i)
+        end
+    end
+else
+    data = {}
+end
+
+data[CONTEXT] = tool.Name
+comp:SetData("ContextTool.contexts", data)
+-- dump(comp:GetData("ContextTool.contexts"))
+
+print(tool.Name ..' is set as context view #'..CONTEXT)
+tool.Comments = 'Context ' .. CONTEXT
