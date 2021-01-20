4748af8b39573cbb2be2f44c7a3f0883aefd8bc7
Alexey Bogomolov <mail@abogomolov.com>
2021-01-20 15:59:23 +0300

    cleanup

--- Scripts/Comp/InstanceTools/RepalceInstances.lua
+++ Scripts/Comp/InstanceTools/RepalceInstances.lua
@@ -10,18 +10,7 @@ function hasKey(tab, value)
     return false
 end
 
-function hasValue(tab, value) 
-    for key, val in pairs(tab) do
-        if val == value then
-            return true
-        end
-    end
-    return false
-end
-
 function getOutputs(tool)
-    inputList = {}
-    index = 1
     for i, inp in ipairs(tool:GetInputList()) do
         if inp:GetAttrs().INPB_Connected then
             return inp:GetConnectedOutput()
@@ -31,7 +20,6 @@ end
 
 function getInputs(tool)
     outputList = {}
-
     index = 1
     while true do
         out = tool:FindMainOutput(index)
@@ -77,10 +65,9 @@ function process()
             flow:Select(tool)
         end
     end
-    
     sourceComp:Copy()
     -- optionally close source comp
-    -- sourceComp:Close() 
+    -- sourceComp:Close()
 
     -- load destination comp
     destComp = fu:LoadComp(compName, true)
@@ -88,9 +75,7 @@ function process()
     destTools, inps, outs = unpack(deleteAndGetPositions(destComp))
     -- paste source tools 
     destComp:Paste()
-    
     destflow = destComp.CurrentFrame.FlowView
-    
 
     for i, tool in ipairs(destComp:GetToolList(true)) do
         sourceTool = hasKey(destTools, tool.Name)
