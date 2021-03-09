b20d68ec92c19eac6bc95aac39e2bdc7aedae572
Alexey Bogomolov <mail@abogomolov.com>
2021-03-09 17:46:18 +0300

    update list loaders, delete some configs

--- Scripts/Comp/DEV/list_loaders.lua
+++ Scripts/Comp/DEV/list_loaders.lua
@@ -1,1 +1,4 @@
+print("\n___________loaders__________")
 for _, ld in pairs(bmd.GetLoaders(comp)) do print(ld.Name, ' ', ld.Clip[1]) end
+print("\n___________savers__________")
+for _, ld in pairs(bmd.GetSavers(comp)) do print(ld.Name, ' ', ld.Clip[1]) end
