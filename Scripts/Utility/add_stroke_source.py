0e95f50c7d897aff18c0894080ce7e8e8dad3bb3
Alex Bogomolov <mail@abogomolov.com>
2021-09-18 01:36:44 +0300

    process comp file, fix strokes source

---
+++ Scripts/Utility/add_stroke_source.py
@@ -0,0 +1,29 @@
+import re
+import sys
+import os
+
+
+def process(file_path, tool_name):
+    with open(file_path, "r") as file:
+        name, ext = os.path.splitext(file_path)
+        out = name + "_out"
+        out_file = out + ext
+        print(out_file)
+        file_read = file.readlines()
+        with open(out_file, "w") as output:
+            source_string = """\t\t\t\t["PaintApplyRubThrough.SourceTool"] = Input {{\n\t\t\t\t\tSourceOp = "{tool_name}",\n\t\t\t\t\tSource = "Output",\n\t\t\t\t}},\n""".format(
+                tool_name=tool_name
+            )
+            for line in file_read:
+                # print(line)
+                result = re.search("ApplyMode =.+PaintApplyRubThrough.*", line)
+                output.write(line)
+                if result:
+                    output.write(source_string)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        file_name, tool_name = sys.argv[1:]
+        file_name = os.path.abspath(file_name)
+        process(file_name, tool_name)
