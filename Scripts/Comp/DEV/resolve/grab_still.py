1b6e3a4b2ca0d3b3bbec9a327140c25f02588049
Alexey Bogomolov <mail@abogomolov.com>
2022-02-14 10:50:59 +0300

    add stills management files

---
+++ Scripts/Comp/DEV/resolve/grab_still.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import os
+import time
+
+"""
+    This is a Davinci Resolve script to save single still in stills folder
+    Author: Alexey Bogomolov
+    Email: mail@abogomolov.com
+    License: MIT
+    Copyright: 2022
+"""
+STILL_FRAME_REF = 2
+
+
+def grab_still():
+    """create stills from all clips in a timeline
+    save the files to requested folder
+    """
+    if not fu.GetResolve():
+        print("This is a script for Davinci Resolve")
+        return
+
+    project = resolve.GetProjectManager().GetCurrentProject()
+    timeline = project.GetCurrentTimeline()
+    timeline_name = timeline.GetName()
+    gallery = project.GetGallery()
+    still = timeline.GrabStill(STILL_FRAME_REF)
+    album = gallery.GetCurrentStillAlbum()
+    target_folder = fu.GetData("ResolveSaveStills.Folder")
+    if not target_folder:
+        target_folder = fu.MapPath(os.path.expanduser("~/Desktop"))
+        print(f"saving stills to {target_folder}")
+    album.ExportStills([still], target_folder, timeline_name, "jpg")
+    # album.DeleteStills(still)
+    resolve.OpenPage("edit")
+    print(f"Saved still to {target_folder}")
+
+
+if __name__ == "__main__":
+    grab_still()
