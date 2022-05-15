c9ee5f121ff42cc60bd88dbdeaa1cb4f97ca5ba0
Alexey Bogomolov <mail@abogomolov.com>
2022-05-16 00:22:25 +0300

    do not use clip trim

--- Scripts/Comp/DEV/CompSetup.py
+++ Scripts/Comp/DEV/CompSetup.py
@@ -112,27 +112,10 @@ def set_range(loader) -> None:
     """set GlobalIn and GlobalOut according to Loader clip length"""
 
     comp.StartUndo("Set Range based on Loader")
-    loader_attrs = loader.GetAttrs()
-
     clip_in = loader.GlobalIn[1]
-    trim_start = loader_attrs["TOOLIT_Clip_TrimIn"][1]
-
     clip_out = loader.GlobalOut[1]
-    trim_end = loader_attrs["TOOLIT_Clip_TrimOut"][1]
-
     comp.SetAttrs({"COMPN_GlobalStart": clip_in})
-    if trim_start == clip_in:
-        comp.SetAttrs({"COMPN_RenderStart": clip_in})
-    else:
-        comp.SetAttrs({"COMPN_RenderStart": trim_start})
-        clip_in = trim_start
     comp.SetAttrs({"COMPN_GlobalEnd": clip_out})
-    if trim_end == clip_out:
-        comp.SetAttrs({"COMPN_RenderEnd": clip_out})
-    else:
-        comp.SetAttrs({"COMPN_RenderEnd": trim_end})
-        clip_out = trim_end
-
     comp.EndUndo()
     print(f"Comp length is adjusted to [ {clip_in} - {clip_out} ]")
 
