3cbf24830547b5f3ce240438662dcf490470005a
Alexey Bogomolov <mail@abogomolov.com>
2023-05-01 04:15:04 +0300

    add fu18 support

--- Scripts/Comp/CleanFlow/Align_V.lua
+++ Scripts/Comp/CleanFlow/Align_V.lua
@@ -1,15 +1,15 @@
 -- The script to align nodes vertically
 -- Based on original script by DC Turner LineSelectedTools.lua
--- Change List by Alex Bogomolov (mail@abogomolov.com):
+-- Updates by Alex Bogomolov (mail@abogomolov.com):
 --  code refactoring
---  floor node positioning check
---  added Fu16 compatibility 
+--  floor node positioning for Fusion 16 and 17 due to the node positioning bug
 --  configuration file to launch the script with ALT+V
---
+
 -- Known issue: if the Node is set to Arrange to grid, and Y pos is less then 0, aligned position will shift to 1 on Y axis
 
 
 flow = comp.CurrentFrame.FlowView
+DEBUG = false
 
 comp:StartUndo("Align Tools Vertically")
 selected_tools = comp:GetToolList(true)
@@ -20,18 +20,21 @@ if active_tool == nil then
 end
 
 active_X, active_Y = flow:GetPos(active_tool)
--- print(active_X.." - ".. active_Y)
+if DEBUG then
+    print(active_X.." - ".. active_Y)
+end
 
-if fu.Version >= 16 then
-    active_Y = math.floor(active_Y)
+if fu.Version >= 16 and fu.Version <= 17 then
+    -- floor position coordinnates for older versions
+    active_X = math.floor(active_X)
     flow:SetPos(active_tool, active_X, active_Y)
 end
 
 for num, tool in pairs(selected_tools) do
-    _TEMP_X, TEMP_Y = flow:GetPos(tool)
+    TEMP_Y = flow:GetPos(tool)[2]
     if tool ~= active_tool then
         -- check if nodes have the same Y value
-        -- floor the values for switching between horiz and vertical allignment
+        -- floor the values to switch between horizonal and vertical allignments
         if math.floor(TEMP_Y) == math.floor(active_Y) then 
             newpos = TEMP_Y + (num-1)
         else
