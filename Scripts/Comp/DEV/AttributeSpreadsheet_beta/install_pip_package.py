3c2021c80e5c205327f7ebd20e03e02026656786
Alexey Bogomolov <mail@abogomolov.com>
2023-06-30 23:05:12 +0300

    refactoring

--- Scripts/Comp/DEV/AttributeSpreadsheet_beta/install_pip_package.py
+++ Scripts/Comp/DEV/AttributeSpreadsheet_beta/install_pip_package.py
@@ -4,19 +4,36 @@ import subprocess
 from pathlib import Path
 
 
-def get_python_executable() -> str:
-    lib_path = Path(os.__file__).parent.parent
+def get_python_home() -> Path:
+    python_home = Path(os.__file__).parent.parent
+    return python_home
+
+
+def get_python_executable(python_home) -> str:
     if platform.system() == "Windows":
-        python_executable = lib_path / "python.exe"
+        python_executable = python_home / "python.exe"
     else:
-        python_executable = lib_path.parent / "bin" / "python3"
+        python_executable = python_home.parent / "bin" / "python3"
     if not python_executable.exists():
-        print("No Python executable found. Please report this.")
+        print(f"No Python executable found at {python_home}")
         return
-    return str(python_executable)
+    return python_executable.as_posix()
 
 
-def output_to_console(process):
+def run_installation_command(python_executable, package):
+    if not python_executable:
+        return
+    command = [
+        python_executable,
+        "-m",
+        "pip",
+        "install",
+        package,
+    ]
+    print(f"running command: {command}")
+    process = subprocess.Popen(
+        command, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
     while True:
         output = process.stdout.readline().decode()
         if output == "" or process.poll() is not None:
@@ -25,33 +42,13 @@ def output_to_console(process):
             print(output.strip())
 
 
-def run_installation_command(package, print_to_console=True):
-    python_executable = get_python_executable()
+def pip_install(package: str, fusion_python_home=None):
+    if fusion_python_home is None:
+        fusion_python_home = get_python_home()
+
+    python_executable = get_python_executable(fusion_python_home)
     if not python_executable:
         return
-    command = [
-        python_executable,
-        "-m",
-        "pip",
-        "install",
-        f"{package}",
-    ]
-    print(f"running command: {command}")
-    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    if print_to_console:
-        output_to_console(process)
-    rc = process.poll()
-    return rc
-
-
-def pip_install(packages: list):
-    print("Installing required libraries")
-    for package in packages:
-        rc = run_installation_command(package)
-        if rc:
-            print(
-                f"{package} installation has failed for some reason"
-                "\nCheck if internet connection is available."
-                "\nPlease report this issue here: https://www.steakunderwater.com/wesuckless/viewtopic.php?t=4529"
-            )
-    print("Now relaunch the script, please")
+    print(f"Installing {package}")
+    run_installation_command(python_executable, package)
+    print("Done.")
