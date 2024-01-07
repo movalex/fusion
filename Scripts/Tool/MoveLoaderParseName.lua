1f723c5f0ed810f1ba9a537ee7c585fee343dffc
Alexey Bogomolov <mail@abogomolov.com>
2024-01-07 04:07:00 +0300

    move to IN and try parse file if no IN found

--- Scripts/Tool/MoveLoaderParseName.lua
+++ Scripts/Tool/MoveLoaderParseName.lua
@@ -1,22 +1,58 @@
-function ParseInPoint(loader)
-    local parsedName = bmd.parseFilename(loader.Clip[1])
-    return parsedName.Number
+
+local function ParseFilename(filename)
+    local seq = {}
+    seq.FullPath = filename
+    string.gsub(seq.FullPath, "^(.+[/\\])(.+)", function(path, name)
+        seq.Path = path
+        seq.FullName = name
+    end)
+    string.gsub(seq.FullName, "^(.+)(%..+)$", function(name, ext)
+        seq.Name = name
+        seq.Extension = ext
+    end)
+    -- check if no extension
+    if not seq.Name then
+        seq.Name = seq.FullName
+    end
+    
+    string.gsub(seq.Name, "^(.-)(%d+)$", function(name, SNum)
+        seq.CleanName = name
+        seq.SNum = SNum
+    end)
+    
+    if seq.SNum then
+        seq.Number = tonumber(seq.SNum)
+        seq.Padding = string.len(seq.SNum)
+    else
+        seq.SNum = ""
+        seq.CleanName = seq.Name
+    end
+    
+    if seq.Extension == nil then
+        seq.Extension = ""
+    end
+    return seq 
 end
 
 
-function moveLoader(tool, startFrom)
+function moveLoader(tool)
     if tool.ID ~= "Loader" then
-        print('this is not a Loader tool')
+        print('This is not a Loader tool')
         return nil
     end
-    comp:StartUndo('set loader start')
-    bmd.MoveClip(tool, comp:GetAttrs().COMPN_GlobalStart, tonumber(startFrom))
+    globalStart = comp:GetAttrs().COMPN_GlobalStart
+    inPoint = comp:GetAttrs().COMPN_RenderStart
+    comp:StartUndo('Move Loader')
+    
+    clipName = tool.Clip[1]
+
+    if inPoint == globalStart then
+        inPoint = ParseFilename(clipName).Number
+    end
+    bmd.MoveClip(tool, globalStart, tonumber(inPoint))
     comp:EndUndo()
 end
 
-inPoint = comp:GetAttrs().COMPN_RenderStart
-parsedInPoint = ParseInPoint(tool)
--- newStart = comp:GetData("FrameHandles.offset")
 
-moveLoader(tool, parsedInPoint)
+moveLoader(tool)
 
