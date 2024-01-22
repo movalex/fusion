ec7b61e97810d7d72b79bd07b5c49231771e9388
Alexey Bogomolov <mail@abogomolov.com>
2024-01-22 04:42:52 +0300

    use mistune to create bbcode

--- Scripts/Utility/md2Reactor.py
+++ Scripts/Utility/md2Reactor.py
@@ -6,21 +6,143 @@ import os
 from pathlib import Path
 
 DEFAULT_DIR = Path("~/Desktop").expanduser()
-PKG_REQUIRED = "markdown"
+PKG_REQUIRED = ["markdown", "mistune"]
 
 try:
     from markdown import markdown
+    import mistune
+    from mistune import BaseRenderer, Markdown
+    from tkinter import Tk, filedialog
 except ModuleNotFoundError:
     script_path = comp.MapPath("Reactor:Deploy/Scripts/Utility")
     sys.path.append(script_path)
     from install_pip_package import pip_install
-    pip_install(PKG_REQUIRED)
+
+    for package in PKG_REQUIRED:
+        pip_install(package)
     sys.exit()
 
 
-def request_file_names(init_dir: str) -> list:
-    from tkinter import filedialog, Tk
+class BBCodeRenderer(BaseRenderer):
+    def blank_line(self, *args, **kwargs):
+        return ""
 
+    def heading(self, text, level, **attrs):
+        # {'type': 'heading', 'attrs': {'level': 1}, 'style': 'axt', 'children': [{'type': 'text', 'raw': 'First Level'}]}
+        levels = {1: "size=200", 2: "size=150", 3: "size=100"}
+        _level = text["attrs"]["level"]
+        heading_size = levels[_level]
+        raw_text = None
+        if "children" in text:
+            for child in text["children"]:
+                if "type" in child and child["type"] == "text" and "raw" in child:
+                    raw_text = child["raw"]
+                    break
+        return f"[{heading_size}][b]{raw_text}[/b][/size]\n"
+
+    def parse_element_to_bbcode(self, element):
+        if element["type"] == "paragraph":
+            bbcode = ""
+            for child in element["children"]:
+                bbcode += self.parse_element_to_bbcode(child)
+            return bbcode + "\n"
+        elif element["type"] == "link":
+            url = element["attrs"]["url"]
+            bbcode = f"[url={url}]"
+            for child in element["children"]:
+                bbcode += self.parse_element_to_bbcode(child)
+            bbcode += "[/url]"
+            return bbcode
+        elif element["type"] == "image":
+            url = element["attrs"]["url"]
+            bbcode = f"[img]{url}[/img]"
+            return bbcode
+        elif element["type"] == "block_quote":
+            bbcode = "[quote]"
+            for child in element["children"]:
+                bbcode += self.parse_element_to_bbcode(child)
+            bbcode += "[/quote]"
+            return bbcode
+        elif element["type"] == "codespan":
+            bbcode = "[c]" + element["raw"] + "[/c]"
+            return bbcode
+        elif element["type"] == "emphasis":
+            bbcode = "[i]"
+            for child in element["children"]:
+                bbcode += self.parse_element_to_bbcode(child)
+            bbcode += "[/i]"
+            return bbcode
+        elif element["type"] == "strong":
+            bbcode = "[b]"
+            for child in element["children"]:
+                bbcode += self.parse_element_to_bbcode(child)
+            bbcode += "[/b]"
+            return bbcode
+        elif element["type"] == "double_emphasis":
+            bbcode = "[b]"
+            for child in element["children"]:
+                bbcode += self.parse_element_to_bbcode(child)
+            bbcode += "[/b]"
+            return bbcode
+        elif element["type"] == "text":
+            return element["raw"]
+        else:
+            return ""
+
+    def paragraph(self, text, state=None):
+        return self.parse_element_to_bbcode(text)
+
+    def extract_and_format_list(self, element):
+        # Initialize an empty list to hold the raw text items
+        list_items = []
+
+        # Check if the 'children' key exists and is a list
+        if "children" in element and isinstance(element["children"], list):
+            # Loop through each child (which should be a list item)
+            for child in element["children"]:
+                # Recursively call this function if the child has its own 'children'
+                if "children" in child:
+                    list_items.extend(self.extract_and_format_list(child))
+                # If the child is a 'text' type, append the 'raw' text to the list_items
+                elif child.get("type") == "text" and "raw" in child:
+                    list_items.append(child["raw"])
+        # Return the list of raw text items
+        return list_items
+
+    def list(self, body, ordered, **attrs):
+        print(f"is ordered: {ordered.cursor}")
+        print(dir(ordered))
+        tag = "=1" if ordered else ""
+        # Extract the raw text from the data structure
+        list_items = self.extract_and_format_list(body)
+        # Format the list items with BBCode syntax
+        formatted_list = (
+            f"[list{tag}]\n"
+            + "\n".join(f"[*]{item}" for item in list_items)
+            + "\n[/list]\n"
+        )
+
+        return formatted_list
+
+    def block_code(self, code, info=None):
+        # {'type': 'block_code', 'raw': 'print("Hello, world!")\n', 'style': 'fenced', 'marker': '```', 'attrs': {'info': 'python'}}
+        raw_code = code["raw"]
+        syntax = code["attrs"].get("info", "")
+        return f"[code]{raw_code}[/code]\n"
+
+    def block_quote(self, text, state=None):
+        return self.parse_element_to_bbcode(text)
+
+    def link(self, link, title, text):
+        return "[url={link}]{text}[/url]".format(link=link, text=text)
+
+    def image(self, src, title, text):
+        return "[img]{src}[/img]".format(src=src)
+
+    # Add more methods as needed to cover the Markdown features you use
+
+
+def request_file_names(init_dir: str) -> list:
     root = Tk()
     root.withdraw()
     md_files = filedialog.askopenfilenames(
@@ -36,6 +158,16 @@ def translate(pattern: str, g=1):
         return pattern.format(s)
 
     return inline
+
+
+def mistune_to_bbcode(markdown_text):
+    # Initialize the Markdown parser with our BBCode renderer
+    renderer = BBCodeRenderer()
+    markdown_parser = Markdown(renderer=renderer)
+
+    # Convert Markdown to BBCode
+    bbcode_text = markdown_parser(markdown_text)
+    return bbcode_text
 
 
 def markdown_to_bbcode(s):
@@ -66,7 +198,7 @@ def markdown_to_bbcode(s):
 
 
 def write_file(file_name, text, suffix):
-    with open(file_name + suffix, "w") as out:
+    with open(file_name + suffix, "w", encoding="utf-8") as out:
         out.write(text)
         print(f"[md2Reactor] created {out.name}")
 
@@ -76,13 +208,13 @@ def main(file_path):
         print("no markdown file_path selected")
         return
 
-    with open(file_path, "r") as fp:
+    with open(file_path, "r", encoding="utf-8") as fp:
         text = fp.read()
 
     file_name, _ = os.path.splitext(file_path)
 
-    bbcode_text = markdown_to_bbcode(text)
-    write_file(file_name, bbcode_text, "_bbcode.txt")
+    bbcode_text = mistune_to_bbcode(text)
+    write_file(file_name, bbcode_text, "_mistune_bbcode.txt")
 
     atom_text = markdown(text)
     if atom_text:
@@ -103,9 +235,10 @@ def process(file_list):
 
 
 if __name__ == "__main__":
-    comp = fu.GetCurrentComp()
-    folder = fu.GetData("md2reactor.path")
-    if not folder:
-        folder = DEFAULT_DIR
-    file_paths = request_file_names(folder)
-    process(file_paths)
+    # comp = fu.GetCurrentComp()
+    # folder = fu.GetData("md2reactor.path")
+    # if not folder:
+    #     folder = DEFAULT_DIR
+    # file_paths = request_file_names(folder)
+    # process(file_paths)
+    main(Path("/Users/videopro/Desktop/test note.md"))
