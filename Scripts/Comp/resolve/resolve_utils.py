5f71d3b26b96b35eb2738db83cabacf0708deabe
Alexey Bogomolov <mail@abogomolov.com>
2024-01-30 02:21:05 +0300

    use base class dialog

--- Scripts/Comp/resolve/resolve_utils.py
+++ Scripts/Comp/resolve/resolve_utils.py
@@ -47,16 +47,50 @@ def get_timeline_and_gallery(resolve):
     return timeline, gallery
 
 
-class ConfirmationDialog:
+class BaseDialog:
+    def __init__(self, fusion, window_title, geometry, id="DefaultID"):
+        self.fusion = fusion
+        print(fusion)
+        self.ui = fusion.UIManager
+        self.id = id
+        self.disp = bmd.UIDispatcher(self.ui)
+        self.window_title = window_title
+        self.geometry = geometry
+        self.itm = None
+        self.win = self.create_window(self.geometry)
+
+    def create_window(self, geometry):
+        self.win = self.disp.AddWindow(
+            {
+                "ID": self.id,
+                "TargetID": self.id,
+                "WindowTitle": self.window_title,
+                "Geometry": geometry,
+            },
+            self.layout(),  # This method should be implemented in subclasses to define the layout
+        )
+        return self.win
+
+    def layout(self, *args):
+        # Placeholder for layout, should be overridden in subclasses
+        raise NotImplementedError
+
+    def show(self):
+        self.win.Show()
+        self.disp.RunLoop()
+        self.win.Hide()
+
+    def close(self, ev):
+        self.disp.ExitLoop()
+
+
+class ConfirmationDialog(BaseDialog):
     def __init__(self, fusion, title=None, request=None):
-        self.confirmed = False
         self.title = "Confirmation Dialog" if title is None else title
         self.request = request
+        geometry = [800, 600, 550, 130]
+        super().__init__(fusion, self.title, geometry, id="ConfirmationDialog")
         self.confirmed = False
-
-        self.ui = fusion.UIManager
-        self.disp = bmd.UIDispatcher(self.ui)
-        self.win = self.create_window()
         self.setup_callbacks()
 
     def setup_callbacks(self):
@@ -65,128 +99,104 @@ class ConfirmationDialog:
         self.win.On.RequestFolder.Close = self.close
 
     def run(self):
-        self.win.Show()
-        self.disp.RunLoop()
-        self.win.Hide()
+        self.show()
         return self.confirmed
-
-    def close(self, ev):
-        self.disp.ExitLoop()
 
     def request_confirmed(self, ev):
         self.confirmed = True
         self.disp.ExitLoop()
 
-    def create_window(self):
-        return self.disp.AddWindow(
-            {
-                "ID": "RequestFolder",
-                "TargetID": "RequestFolder",
-                "WindowTitle": "Confirmation Dialogue",
-                "Geometry": [800, 600, 550, 130],
-            },
+    def layout(self):
+        return [
+            self.ui.VGroup(
+                [
+                    self.ui.Label(
+                        {
+                            "ID": "TitleLabel",
+                            "Text": f"<H2>{self.title}<H2/>",
+                            "Weight": 0.5,
+                            "Alignment": {
+                                "AlignHCenter": True,
+                                "AlignVCenter": True,
+                            },
+                        }
+                    ),
+                    self.ui.Label(
+                        {
+                            "ID": "RequestLabel",
+                            "Text": self.request,
+                            "Weight": 0.5,
+                            "Alignment": {
+                                "AlignHCenter": True,
+                                "AlignVCenter": True,
+                            },
+                        }
+                    ),
+                    self.ui.HGroup(
+                        {"Alignment": {"AlignHRight": True}, "Weight": 0},
+                        [
+                            self.ui.Button(
+                                {
+                                    "ID": "OkButton",
+                                    "Text": "Ok",
+                                }
+                            ),
+                            self.ui.Button(
+                                {
+                                    "ID": "CancelButton",
+                                    "Text": "Cancel",
+                                }
+                            ),
+                        ],
+                    ),
+                ]
+            ),
+        ]
+
+
+class RequestDir(BaseDialog):
+    def __init__(self, fusion, title=None, target=None):
+        self.title = "Choose Directory" if title is None else title
+        geometry = [800, 700, 630, 50]
+        self.target = target or Path("~/Desktop").expanduser().absolute()
+        super().__init__(fusion, self.title, geometry, id="RequestFolderDialog")
+        self.itm = self.win.GetItems()
+        self.setup_callbacks()
+
+    def setup_callbacks(self):
+        self.win.On.FolderButton.Clicked = self.request_folder
+        self.win.On.RequestFolderDialog.Close = self.close
+        self.win.On.SaveButton.Clicked = self.close
+
+    def layout(self):
+        return self.ui.HGroup(
             [
-                self.ui.VGroup(
-                    [
-                        self.ui.Label(
-                            {
-                                "ID": "TitleLabel",
-                                "Text": f"<H2>{self.title}<H2/>",
-                                "Weight": 0.5,
-                                "Alignment": {
-                                    "AlignHCenter": True,
-                                    "AlignVCenter": True,
-                                },
-                            }
-                        ),
-                        self.ui.Label(
-                            {
-                                "ID": "RequestLabel",
-                                "Text": self.request,
-                                "Weight": 0.5,
-                                "Alignment": {
-                                    "AlignHCenter": True,
-                                    "AlignVCenter": True,
-                                },
-                            }
-                        ),
-                        self.ui.HGroup(
-                            {"Alignment": {"AlignHRight": True}, "Weight": 0},
-                            [
-                                self.ui.Button(
-                                    {
-                                        "ID": "OkButton",
-                                        "Text": "Ok",
-                                    }
-                                ),
-                                self.ui.Button(
-                                    {
-                                        "ID": "CancelButton",
-                                        "Text": "Cancel",
-                                    }
-                                ),
-                            ],
-                        ),
-                    ]
-                ),
-            ],
-        )
-
-
-def request_dir(window_title: str, target=None):
-    """request file UI"""
-
-    fu = get_fusion_module()
-    if target is None:
-        target = Path("~/Desktop").expanduser().absolute()
-
-    ui = fu.UIManager
-    disp = bmd.UIDispatcher(ui)
-    win = disp.AddWindow(
-        {
-            "ID": "RequestFolder",
-            "TargetID": "RequestFolder",
-            "WindowTitle": window_title,
-            "Geometry": [800, 600, 630, 50],
-        },
-        ui.HGroup(
-            [
-                ui.Label({"Weight": 0.1, "ID": "FolderLabel", "Text": "folder:"}),
-                ui.LineEdit(
+                self.ui.Label({"Weight": 0.1, "ID": "FolderLabel", "Text": "folder:"}),
+                self.ui.LineEdit(
                     {
                         "Weight": 0.8,
-                        "Text": str(target) or "",
+                        "Text": str(self.target),
                         "ID": "FolderLine",
                         "Events": {"ReturnPressed": True},
                         "Alignment": {"AlignHCenter": True, "AlignVCenter": True},
                     }
                 ),
-                ui.Button({"Weight": 0.2, "ID": "FolderButton", "Text": "Browse..."}),
-                ui.Button({"Weight": 0.1, "ID": "RunButton", "Text": "Save"}),
+                self.ui.Button(
+                    {"Weight": 0.2, "ID": "FolderButton", "Text": "Browse..."}
+                ),
+                self.ui.Button({"Weight": 0.1, "ID": "SaveButton", "Text": "Save"}),
             ]
-        ),
-    )
-    itm = win.GetItems()
+        )
 
-    def close(ev):
-        disp.ExitLoop()
+    def request_folder(self, ev):
+        target_folder = self.fusion.RequestDir()
+        self.itm["FolderLine"].Text = target_folder
 
-    def request_folder(ev):
-        target_folder = fu.RequestDir()
-        itm["FolderLine"].Text = target_folder
-
-    itm["FolderLine"].SetPlaceholderText("Select folder")
-    win.On.FolderButton.Clicked = request_folder
-    win.On.RequestFolder.Close = close
-    win.On.RunButton.Clicked = close
-    win.Show()
-    disp.RunLoop()
-    win.Hide()
-    result = itm["FolderLine"].Text
-    print(f"result: '{result}'")
-
-    return result
+    def run(self):
+        self.show()
+        return self.itm["FolderLine"].Text
 
 
 if __name__ == "__main__":
-    request_dir()
+    fusion = get_fusion_module()
+    RequestDir(fusion).run()
