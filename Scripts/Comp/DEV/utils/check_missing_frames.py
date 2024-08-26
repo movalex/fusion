d9e0de89e7dfd8d835bd4ffd4b5f9c145901d70f
Alexey Bogomolov <mail@abogomolov.com>
2024-08-26 23:06:16 +0300

    process selected loader, refactoring

--- Scripts/Comp/DEV/utils/check_missing_frames.py
+++ Scripts/Comp/DEV/utils/check_missing_frames.py
@@ -9,6 +9,7 @@
 #   v1.1 -- bug fixing, add frames number concatenation
 #   v1.2 -- use file range instead of comp range
 #   v1.3 -- work correctly with multiple sequences in the same folder
+#   v1.4 -- process selected loader, refactoring
 
 
 import re
@@ -16,7 +17,7 @@ import mimetypes
 from pathlib import Path
 
 comp = fu.GetCurrentComp()
-VERSION = 1.3
+VERSION = 1.4
 
 
 def is_movie_format(file_name):
@@ -85,58 +86,84 @@ def get_line_numbers_concat(line_nums):
     final_str = ', '.join(final)
     return final_str
 
+def scan_for_missing_frames(tool):
+    file_path = comp.MapPath(tool.Clip[fu.TIME_UNDEFINED])
+    print(file_path)
+    if not tool.ID == "Loader":
+        print("Not a loader")
+        return
+    if not file_path or is_movie_format(file_path):
+        print(f"File path is not found in {tool.Name} or {loader.Name} is not a sequence format")
+        return
+    clip_attrs = tool.GetAttrs()
+    try:
+        start_frame = int(clip_attrs["TOOLIT_Clip_StartFrame"][1])
+        clip_length = int(clip_attrs["TOOLIT_Clip_Length"][1])
+    except KeyError:
+        print(f"skipping Loader [{tool.Name}] with missing clip attributes")
+        return
+    if clip_length < 2:
+        if loader.Loop[fu.TIME_UNDEFINED]:
+            print(f"{file_path} is a single frame Loader. Skipping")
+        else:
+            print(f"{file_path} has single frame an not set to loop. Skipping")
+        return
+    print(f"Clip Length: {clip_length}")
+    path = Path(file_path)
+    ext = path.suffix
+    print(f"Extension: {ext}")
+    parent_dir = path.parent
+    print(f"Parent Folder: {parent_dir}")
+    stem = re.sub("\d+$", "", path.stem)
+    print(f"Path Stem: {path.stem}")
+    print(f"File Stem: {stem}")
+    # Use pathlib's glob to find files in the directory
+    all_files = parent_dir.glob('*')
+
+    pattern = re.compile(f'{stem}\d+{ext}$')
+    target_sequence = [str(f) for f in all_files if pattern.match(f.name)]
+    # Filter files matching the pattern
+    seq_pattern = re.compile(r"(\d{3,})" + ext + "$", re.IGNORECASE)
+    sequence_length = len(target_sequence)
+    print(f"SEQ Length: {sequence_length}")
+    if sequence_length < clip_length:
+        # print(f"{path}: \nMissing frames:")
+        frames_found = get_frame_nums(target_sequence, seq_pattern)
+        missing_frames = [i for i in range(start_frame, start_frame + clip_length) if i not in frames_found]
+        print("-" * 18)
+        print(get_line_numbers_concat(missing_frames))
+        even_numbers = [i for i in missing_frames if i % 2 == 0]
+        print("-" * 18)
+        print(f"Found missing even frames: {even_numbers}")
+        return True
+
 
 def scan_all_loaders():
-    failed = 0
+    missing_counter = 0
     loaders = comp.GetToolList(False, "Loader")
+    if not loaders:
+        print("No loaders found")
+        return
     total = len(loaders)
     if total == 0:
-        print("no loaders found in the comp")
+        print("No loaders found in the comp")
         return
 
-    for tool in loaders.values():
-        file_path = comp.MapPath(tool.Clip[fu.TIME_UNDEFINED])
-        # print(file_path)
-        if not file_path or is_movie_format(file_path):
-            # print(f"File path is not found in {tool.Name} or {tool.Name} is not a sequence format")
-            continue
-        clip_attrs = tool.GetAttrs()
-        try:
-            start_frame = int(clip_attrs["TOOLIT_Clip_StartFrame"][1])
-            clip_length = int(clip_attrs["TOOLIT_Clip_Length"][1])
-        except KeyError:
-            print(f"skipping Loader [{tool.Name}] with missing clip attributes")
-            continue
-        if clip_length < 2:
-            if tool.Loop[fu.TIME_UNDEFINED]:
-                print(f"{file_path} is a single frame Loader. Skipping")
-            else:
-                print(f"{file_path} has single frame an not set to loop. Skipping")
-            continue
-        path = Path(file_path)
-        ext = path.suffix
-        parent_dir = path.parent
-        stem = re.sub("\d+$", "", path.stem)
-        seq = list(parent_dir.glob(f"*{stem}*{ext}"))
-        seq_pattern = re.compile(r"(\d{3,})" + ext + "$", re.IGNORECASE)
-        sequence_length = len(seq)
-        if sequence_length < clip_length:
-            print(f"{path}: File length mismatch found!\nMissing frames:")
-            frames_found = get_frame_nums(seq, seq_pattern)
-            missing_frames = [i for i in range(start_frame, start_frame + clip_length) if i not in frames_found]
-            print("-" * 18)
-            print(get_line_numbers_concat(missing_frames))
-            print("-" * 18)
-            failed += 1
+    for loader in loaders.values():
+        counter = scan_for_missing_frames(loader)
+        if counter:
+            missing_counter += 1
 
-    print(f"found {failed} loaders with mismatched frames")
+
+    print(f"found {missing_counter} loaders with missing frames")
 
 
 
 if __name__ == "__main__":
     print(f"Check Missing Frames script. Version {VERSION}\nCopyright 2021 Alexey Bogomolov. MIT License")
-    try:
+    active_tool = comp.ActiveTool
+    if active_tool:
+        scan_for_missing_frames(active_tool)
+    else:
         scan_all_loaders()
-    except AttributeError:
-        print("no loaders found")
     print("Done!")
