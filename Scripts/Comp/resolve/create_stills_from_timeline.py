0013e40983a56e9cf2bbaab129c03c0dfb42f215
Alexey Bogomolov <mail@abogomolov.com>
2024-08-27 03:58:10 +0300

    use utils

--- Scripts/Comp/resolve/create_stills_from_timeline.py
+++ Scripts/Comp/resolve/create_stills_from_timeline.py
@@ -1,11 +1,8 @@
 from pathlib import Path
 from grab_still import get_still_album, get_target_folder
-from resolve_utils import (
-    get_current_timeline,
-    get_gallery,
+import DaVinciResolveScript as dvr_script
+from UI_utils import (
     ConfirmationDialog,
-    create_folder,
-    cleanup_drx,
 )
 
 """
@@ -16,12 +13,31 @@ from resolve_utils import (
     Email: mail@abogomolov.com
 """
 
-STILL_FRAME_REF = 2
-STILL_ALBUM = "Export"
-FORMAT = "png"
+STILL_FRAME_REF = 2  # 1 - First frame, 2 - Middle frame
+STILL_ALBUM = "STILLS"
+STILL_FORMAT = "png"
 DELETE_STILLS = True
 OPEN_EDIT_PAGE_AFTER_EXPORT = True
 CLEANUP_DRX = True
+
+
+resolve = dvr_script.scriptapp("Resolve")
+utils = ResolveUtility(resolve)
+
+
+def cleanup_drx_from_folder(folder: Path):
+    print(
+        "CLEANUP DRX is enabled, all .drx files in the stills location will be erased"
+    )
+    answer = ConfirmationDialog(
+        title="DRX files deletion!",
+        request=f"Do you wish to delete all DRX files\nin'{folder}' folder?",
+    )
+
+    if answer:
+        for file in folder.iterdir():
+            if file.suffix == ".drx":
+                file.unlink()
 
 
 def post_processing(stills: list, still_album, gallery, target_folder=None):
@@ -30,17 +46,15 @@ def post_processing(stills: list, still_album, gallery, target_folder=None):
         print(
             f"Please note, that DELETE_STILLS is set to True, so new stills in '{still_album_name}' album will be erased"
         )
-        dialog = ConfirmationDialog(
-            fu,
+        answer = ConfirmationDialog(
             "Stills Deletion!",
             f"Do you want to delete stills\nin '{still_album_name}' album?",
         )
-        answer = dialog.run()
         if answer:
             still_album.DeleteStills(stills)
 
     if CLEANUP_DRX:
-        cleanup_drx(target_folder)
+        cleanup_drx_from_folder(target_folder)
 
     if OPEN_EDIT_PAGE_AFTER_EXPORT:
         resolve.OpenPage("edit")
@@ -55,9 +69,9 @@ def grab_timeline_stills(delete_stills=False):
         print("This is a script for Davinci Resolve")
         return
 
-    gallery = get_gallery(resolve)
+    gallery = utils.get_gallery()
 
-    current_timeline = get_current_timeline(resolve)
+    current_timeline = utils.get_current_timeline()
     timeline_name = current_timeline.GetName()
     video_track_count = current_timeline.GetTrackCount("video")
 
@@ -77,11 +91,15 @@ def grab_timeline_stills(delete_stills=False):
         return
 
     target_folder = Path(target_folder, still_album_name)
-    create_folder(target_folder)
+    folder = utils.create_folder(target_folder)
+    if not folder:
+        utils.reset_global_data("ResolveSaveStills.Folder")
 
     print(f"Saving stills to {target_folder}")
 
-    still_album.ExportStills(stills, target_folder.as_posix(), timeline_name, FORMAT)
+    still_album.ExportStills(
+        stills, target_folder.as_posix(), timeline_name, STILL_FORMAT
+    )
     post_processing(stills, still_album, gallery, target_folder)
 
 
