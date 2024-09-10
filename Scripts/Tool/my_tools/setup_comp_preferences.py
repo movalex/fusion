16bebfb4a491a7b3c68b5b115bcf425caaa0843a
Alexey Bogomolov <mail@abogomolov.com>
2024-09-11 02:18:38 +0300

    catch some errors, simplify the  arguments list

--- Scripts/Tool/my_tools/setup_comp_preferences.py
+++ Scripts/Tool/my_tools/setup_comp_preferences.py
@@ -7,6 +7,7 @@
     License: MIT
 """
 
+
 def convert_to_number(data: str, to_type):
     try:
         return to_type(data)
@@ -14,27 +15,32 @@ def convert_to_number(data: str, to_type):
         print(f"The value {data} could not be converted to {to_type.__name__}")
         return None
 
+
 def set_comp_pref(key: str, value):
     comp.SetPrefs(key, value)
 
 
-def process_metadata(metadata, key_fragment, comp_pref_key, convert_func, value_type, key_string=None):
+def process_metadata(
+    metadata, key_fragment, comp_pref_key, key_string=None
+):
     if not key_string:
-        key_string = comp_pref_key.split('.')[-1]
+        key_string = comp_pref_key.split(".")[-1]
     for key, value in metadata.items():
         if key_fragment in key.lower():
-            converted_value = convert_func(value, value_type)
+            converted_value = convert_to_number(value, float)
             if converted_value is not None:
                 set_comp_pref(comp_pref_key, converted_value)
                 print(f"{key_string} is set to {converted_value}")
                 return True
     return False
 
+
 def set_from_background(tool):
     width = tool.Width[fu.TIME_UNDEFINED]
     height = tool.Height[fu.TIME_UNDEFINED]
     aspect_x, aspect_y, _ = tool.PixelAspect[fu.TIME_UNDEFINED].values()
     return width, height, aspect_x, aspect_y
+
 
 def set_from_loader(tool):
     attrs = tool.GetAttrs()
@@ -44,9 +50,12 @@ def set_from_loader(tool):
         aspect_x = attrs["TOOLN_ImageAspectX"]
         aspect_y = attrs["TOOLN_ImageAspectY"]
     except KeyError:
-        print("To set default pixel aspect ratio, add the Loader to the viewer")
+        print(
+            "Could not read pixel aspect ratio\nAdd the Loader to the viewer and try again"
+        )
         aspect_x = aspect_y = None
     return width, height, aspect_x, aspect_y
+
 
 def set_comp_size_and_aspect(width, height, aspect_x=None, aspect_y=None):
     set_comp_pref("Comp.FrameFormat.Width", width)
@@ -57,6 +66,7 @@ def set_comp_size_and_aspect(width, height, aspect_x=None, aspect_y=None):
         set_comp_pref("Comp.FrameFormat.AspectY", aspect_y)
         print("Comp pixel aspect is set to {}:{}".format(aspect_x, aspect_y))
     set_comp_pref("Comp.FrameFormat.Name", "Set by Script")
+
 
 def main():
     tool = comp.ActiveTool
@@ -70,16 +80,24 @@ def main():
     elif tool.ID == "Background":
         width, height, aspect_x, aspect_y = set_from_background(tool)
         set_comp_size_and_aspect(width, height, aspect_x, aspect_y)
-
-    tool_metadata = tool.Output[comp.CurrentTime].Metadata
+    try:
+        tool_metadata = tool.Output[comp.CurrentTime].Metadata
+    except AttributeError:
+        print(
+            "Could not read tool's metadata.\nAdd the tool to the viewer and try again"
+        )
     if not tool_metadata:
         print("No metadata found")
         return
     fps_found = process_metadata(
-        tool_metadata, "fps", "Comp.FrameFormat.Rate", convert_to_number, float, key_string="FrameRate"
+        tool_metadata,
+        "fps",
+        "Comp.FrameFormat.Rate",
+        key_string="FrameRate",
     )
     if not fps_found:
         print("No framerate information found in metadata")
+
 
 if __name__ == "__main__":
     main()
