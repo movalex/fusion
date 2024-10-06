fa46d30ed3b10c4614fbc8e7b12066655a987925
Alexey Bogomolov <mail@abogomolov.com>
2024-10-06 12:16:23 +0300

    use optional timeline pattern TODO: add ui

--- Scripts/Comp/resolve/rename_timelines.py
+++ Scripts/Comp/resolve/rename_timelines.py
@@ -1,20 +1,25 @@
-def rename_timelines():
+from resolve_utils import ResolveUtility
+
+utils = ResolveUtility()
+
+
+def rename_timelines(pattern):
     """
     rename timelines with a pattern
     """
 
-    project = resolve.GetProjectManager().GetCurrentProject()
+    project = utils.get_current_project()
     timeline_count = project.GetTimelineCount()
     counter = 0
     for n in range(1, timeline_count + 1):
         timeline = project.GetTimelineByIndex(n)
         timeline_name = timeline.GetName()
         print(timeline_name)
-        if timeline_name.startswith("tl"):
+        if timeline_name.startswith(pattern):
             counter += 7
             new_name = f"timeline_{counter:03}"
             timeline.SetName(new_name)
 
 
 if __name__ == "__main__":
-    rename_timelines()
+    rename_timelines("tl")
