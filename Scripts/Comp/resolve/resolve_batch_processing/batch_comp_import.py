b647919aa7c577328a5d15884dba0d1131905fdf
Alexey Bogomolov <mail@abogomolov.com>
2024-11-15 16:52:55 +0300

    import comps to the edit

---
+++ Scripts/Comp/resolve/resolve_batch_processing/batch_comp_import.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+"""WIP, seems does not work"""
+
+import time
+from UI_utils import ConfirmationDialog
+from resolve_utils import ResolveUtility
+
+utils = ResolveUtility()
+
+def process_clips():
+    """Main function to process all clips in the timeline."""
+    clips = utils.get_clips_in_timeline("Video", 4)
+    if not clips:
+        return
+    
+    answer = ConfirmationDialog("Switch ages", "Do you want to proceed?")
+    if answer:
+        for n, clip in enumerate(clips[1:10]):
+            print(f"Processing clip {n}")
+            clip.ImportFusionComp(r"D:\YandexDisk\Шансон ТВ (общая)\БОГОМОЛОВ Алексей\LOGO\age\fusion\age.comp")
+            time.sleep(1)
+
+
+if __name__ == "__main__":
+    process_clips()
