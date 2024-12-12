cda03f70acc2fc7d59fbf57e9aa97ab41745a30e
Alexey Bogomolov <mail@abogomolov.com>
2024-12-12 18:42:14 +0300

    save without loader in/out settings by default

--- Modules/Python/fusion_comp_utils.py
+++ Modules/Python/fusion_comp_utils.py
@@ -13,19 +13,25 @@ class CompUtils:
             print("No Loader selected")
             return None
 
-    def set_range(self, loader):
+    def set_range(self, loader, use_loader_settings=False):
         """Set GlobalIn and GlobalOut according to Loader clip length."""
         if loader is None:
             print("Loader is not available")
             return
 
         self.comp.StartUndo("Set Range based on Loader")
-        clip_in = loader.GlobalIn[1]
-        clip_out = loader.GlobalOut[1]
-        self.comp.SetAttrs({"COMPN_GlobalStart": clip_in})
-        self.comp.SetAttrs({"COMPN_GlobalEnd": clip_out})
+        if use_loader_settings:
+            in_point = loader.GlobalIn[1]
+            out_point = loader.GlobalOut[1]
+        else:
+            in_point = self.comp.GetAttrs()["COMPN_GlobalStart"]
+            out_point = loader.GetAttrs()["TOOLIT_Clip_Length"][1] - 1
+        self.comp.SetAttrs({"COMPN_GlobalStart": in_point})
+        self.comp.SetAttrs({"COMPN_GlobalEnd": out_point})
+        self.comp.SetAttrs({"COMPN_RenderStart": in_point})
+        self.comp.SetAttrs({"COMPN_RenderEnd": out_point})
         self.comp.EndUndo()
-        print(f"Comp length is adjusted to [ {clip_in} - {clip_out} ]")
+        print(f"Comp length is adjusted to [ {in_point} - {out_point} ]")
 
     def parse_loader_path(self, loader):
         loader_path = Path(self.comp.MapPath(loader.Clip[1]))
