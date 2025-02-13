9c0dc99b7216bd144651059735450b071972b507
Alexey Bogomolov <mail@abogomolov.com>
2025-02-13 04:37:25 +0300

    refactoring, update docs

--- Scripts/Comp/resolve/resolve_batch_processing/adjust_age_template.py
+++ Scripts/Comp/resolve/resolve_batch_processing/adjust_age_template.py
@@ -1,23 +1,7 @@
-#!/usr/bin/env python
-
 """
-    ╔════════════════════════════════════════════════════════════════════╗
-    ║                                                                    ║
-    ║    DaVinci Resolve Multi-Clip Font Switcher                        ║
-    ║                                                                    ║
-    ║    Description:                                                    ║
-    ║      This script automates the process of switching fonts across   ║
-    ║      multiple clips in DaVinci Resolve, allowing for batch changes ║
-    ║      in Fusion compositions and Text+ elements. Ideal for creating ║
-    ║      consistent typography across various versions of a timeline.  ║
-    ║                                                                    ║
-    ║    Author: Alexey Bogomolov                                        ║
-    ║    Contact: mail@abogomolov.com                                    ║
-    ║    License: MIT                                                    ║
-    ║    Copyright © 2022 Alexey Bogomolov                               ║
-    ║                                                                    ║
-    ║                                                                    ║
-    ╚════════════════════════════════════════════════════════════════════╝
+Module: adjust_age_template
+Updates Fusion composition parameters for the "Template" tool used in credits.
+Retrieves clips, asks for user confirmation, and applies font/color modifications.
 """
 
 from UI_utils import ConfirmationDialog
@@ -27,8 +11,7 @@ utils = ResolveUtility()
 FONT_COLOR = 0.2117647058824
 FONT_STYLE = "Microsoft Sans Serif"
 
-
-def process(comp):
+def modify_tools(comp):
     """
     switch font im the credits
     """
@@ -37,28 +20,24 @@ def process(comp):
 
     modifications = {
         "Size": 0.08,
-        # "Font": FONT_STYLE,
-        # "Red3": FONT_COLOR,
-        # "Green3": FONT_COLOR,
-        # "Blue3": FONT_COLOR,
-        # "ShadingMappingAspect1": 1.25,
+        "Font": FONT_STYLE,
+        "Red3": FONT_COLOR,
+        "Green3": FONT_COLOR,
+        "Blue3": FONT_COLOR,
+        "ShadingMappingAspect1": 1.25,
     }
     utils.modify_tool_parameters(comp, tool_name, modifications)
 
-
-def process_clips():
+def main():
     """Main function to process all clips in the timeline."""
     clips = utils.get_clips_in_timeline("Video", 4)
     if not clips:
         return
 
-    answer = ConfirmationDialog(
-        "Switching font", f"Do you want switch font to {FONT_STYLE}?"
-    )
+    answer = ConfirmationDialog("Switching font", f"Do you want switch font to {FONT_STYLE}?")
     if answer:
         for clip in clips:
-            utils.process_fusion_comp(clip, process_functions=[process])
-
+            utils.process_fusion_comp(clip, process_functions=[modify_tools])
 
 if __name__ == "__main__":
-    process_clips()
+    main()
