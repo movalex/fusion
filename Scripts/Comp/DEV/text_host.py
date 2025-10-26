5e285a4c1f3b455ccaa1ef0e0c3c461e4c795697
Alexey Bogomolov <mail@abogomolov.com>
2025-10-26 23:21:38 +0300

    remove old scripts

--- Scripts/Comp/DEV/text_host.py
+++
@@ -1,7 +0,0 @@
-import socket
-host = socket.gethostname()
-text_host = comp.AddTool('TextPlus', -32768, -32768)
-text_host.SetAttrs({'TOOLS_Name': 'computer_host'})
-text_host.StyledText = host
-text_host.Size = .03
-text_host.Center = {1: .1, 2: .1, 3: 0}
